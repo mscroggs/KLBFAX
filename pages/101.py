b2ed5621fdf8bad89ce2d6ac6524189fbc2c6819
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-02-26 13:57:30 +0000

    Added votes

---
+++ pages/101.py
@@ -0,0 +1,19 @@
+from colours import colour_print
+
+page = colour_print("""VOTES
+
+7|
+6|
+5|
+4|
+3|
+2|    x
+1|    x      """)+"""
+   O  B  T  T
+   L  E  O  O
+   L  L  M  M
+   Y  G  1  2
+      I
+      N
+
+"""
