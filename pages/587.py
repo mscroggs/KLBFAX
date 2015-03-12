d21221b5d8929df3d30cff201b81ef679f0a86d2
MScroggs <matthew.w.scroggs@gmail.com>
2015-03-12 19:56:16 +0000

    Added bad tempered cake recipe

---
+++ pages/587.py
@@ -0,0 +1,34 @@
+#width:79
+#height: 30
+
+from base_page import Page
+
+recipe_page=Page("587")
+
+recipe_page.content=recipe_page.colours.colour_print("""
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxx      xxx      xx      xx  xx     xxx      xx      xxxxxxxxxxxxxxxxxxxx
+xxxxxxxxx  xx   xx  xxxxxx  xxxxxx  xx  xx  xx  xxxxxx  xxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxx     xxxx    xxxx  xxxxxx  xx     xxx    xxxx      xxxxxxxxxxxxxxxxxxxx
+xxxxxxxxx  xx  xxx  xxxxxx  xxxxxx  xx  xxxxxx  xxxxxxxxxx  xxxxxxxxxxxxxxxxxxxx
+xxxxxxxxx  xxx  xx      xx      xx  xx  xxxxxx      xx      xxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx""",recipe_page.colours.Background.DEFAULT,recipe_page.colours.Foreground.BLUE)+"""
+
+BAD-TEMPERED CAKE (A.K.A. Tiffin)
+
+  1/2 lb    Rich tea and/or digestive biscuits
+    4 oz    Margarine
+    1 dsp   Sugar
+    3 dsp   Cocoa
+    3 dsp   Drinking chocolate
+1 1/2 tbsp  Golden syrup
+    2 oz    Sultanas
+    2 bars  Milk chocolate
+
+1. Break the biscuits.
+2. Melt the margarine, sugar, cocoa, drinking chocolate and syrup over a medium
+   heat.
+3. Mix with biscuits and sultanas and press down in baking tray.
+4. Cover with melted milk chocolate.
+5. Leave in the fridge to set
+"""
