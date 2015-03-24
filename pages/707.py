1992fe30655d1cd8eddfd7b46b7536130c8057ab
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-03-24 10:21:37 +0000

    tweak chalkdust page

--- pages/707.py
+++ pages/707.py
@@ -7,4 +7,8 @@ page_number = os.path.splitext(os.path.basename(__file__))[0]
 chalk_page = Page(page_number)
 chalk_page.title = "chalkdust"
 chalk_page.content = colour_print(
-    printer.text_to_ascii("chalkdust!"))
+    printer.text_to_ascii("!chalkdust!")+"\n"+
+    printer.text_to_ascii("!chalkdust!")+"\n"+
+    printer.text_to_ascii("!chalkdust!")+"\n"+
+    printer.text_to_ascii("!chalkdust!")
+    )
