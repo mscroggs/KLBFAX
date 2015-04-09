080a0505540d1b7df3f7fcc3acec5557e144f7ab
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-04-09 17:23:15 +0100

    Added lots of train stations

--- pages/700.py
+++ pages/700.py
@@ -9,16 +9,18 @@ def strip_tags(string):
     return sub(r'<[^>]*?>', '', string)
 
 class TrainPage(Page):
-    def __init__(self, page_num):
+    def __init__(self, page_num, station, code):
         super(TrainPage, self).__init__(page_num)
         self.title = "Trains"
-        self.tagline = "Live trains from Euston. Data from opentraintimes.com."
+        self.tagline = "Live trains from "+station+". Data from opentraintimes.com."
+        self.station = station
+        self.code = code
 
     def generate_content(self):
         import urllib2
-        content = colour_print(printer.text_to_ascii("TRAINS"))+self.colours.Foreground.YELLOW+self.colours.Background.BLUE+" from Euston"+self.colours.Foreground.DEFAULT+self.colours.Background.DEFAULT
+        content = colour_print(printer.text_to_ascii("TRAINS"))+self.colours.Foreground.YELLOW+self.colours.Background.BLUE+" from "+self.station+self.colours.Foreground.DEFAULT+self.colours.Background.DEFAULT
         content += self.colours.Foreground.GREEN+"\nTime Destination"+" "*49+"Platform\n"+self.colours.Foreground.DEFAULT
-        response = urllib2.urlopen("http://www.opentraintimes.com/location/euston/"+self.now().strftime("%Y-%m-%d/%H:%M")+"?passenger=on&show_call=on&show_stp=on&show_var=on&show_wtt=on&utf8=%E2%9C%93")
+        response = urllib2.urlopen("http://www.opentraintimes.com/location/"+self.code+"/"+self.now().strftime("%Y-%m-%d/%H:%M")+"?passenger=on&show_call=on&show_stp=on&show_var=on&show_wtt=on&utf8=%E2%9C%93")
         html = response.read()
         trains = html.split("<table")[1].split("</table>")[0].split("<tr>")
         for train in trains[2:]:
@@ -30,5 +32,18 @@ class TrainPage(Page):
                 content += new
         self.content = content
 
-page_number = os.path.splitext(os.path.basename(__file__))[0]
-train_page = TrainPage(page_number)
+train01 = TrainPage("701","Euston","EUS")
+train02 = TrainPage("702","Waterloo","WAT")
+train03 = TrainPage("703","Paddington","PAD")
+train04 = TrainPage("704","Banbury","BAN")
+train05 = TrainPage("705","Marylebone","MYB")
+train06 = TrainPage("706","London Bridge","LBG")
+train08 = TrainPage("708","King's Cross","KGX")
+train09 = TrainPage("709","Reading","RDG")
+train10 = TrainPage("710","Oxford","OXF")
+train11 = TrainPage("711","Stratford-upon-Avon","SAV")
+train12 = TrainPage("712","B'ham New Street","BHM")
+train13 = TrainPage("713","B'ham Moor Street","BMO")
+train14 = TrainPage("714","B'ham Snow Hill","BSW")
+train15 = TrainPage("715","Wembley Stadium","WCX")
+train16 = TrainPage("716","Kilmarnock","KMK")
