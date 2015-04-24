d9d030226f7f343eea817e64b3e0a978688fce8c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-04-24 16:05:10 +0100

    u

--- page/NamePage.py
+++ page/NamePage.py
@@ -19,8 +19,8 @@ class NamePage(Page):
         greetings = ["Hi","Hello","Bonjour","Bello","Merhaba",
                      "nuqneH","Hola","Salut","Hallo","Hey",
                      "Yo","Dia dhuit","Salve","Ciao",u"\u4eca\u65e5\u306f",
-                     "Shwmae","\u0417\u0434\u0440\u0430\u0432\u0441\u0442\u0432\u0443\u0439\u0442\u0435!"
-                     "\u4f60\u597d"]
+                     "Shwmae",u"\u0417\u0434\u0440\u0430\u0432\u0441\u0442\u0432\u0443\u0439\u0442\u0435!"
+                     u"\u4f60\u597d"]
 
         greeting = choice(greetings)
         #greeting = greetings[-1]
