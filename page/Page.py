5dd9a2de182ae868f1b5efb7e9597e5e519c251d
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-04-24 18:05:35 +0100

    disabled errors. They make it more like mistake-ridden CEEFAX, but I don't like them.

--- page/Page.py
+++ page/Page.py
@@ -8,13 +8,13 @@ from random import random,randint,choice
 import colours
 
 def random_error(string):
-    if random()<0.03:
+    """if random()<0.03:
       try:
         chars = ["A","5","h","i","#","@","{","9","."]
         pos = randint(0,len(string)-2)
         string = string[:pos]+choice(colours.Foreground.list)+string[pos]+colours.Foreground.DEFAULT+string[pos+1:]
       except:
-        pass
+        pass"""
     return string
 
 
