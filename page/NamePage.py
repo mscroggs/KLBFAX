6cf8a3291b17381d4de22bf9d64ec03a77cc4f7a
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2015-04-24 17:56:19 +0100

    ali g hello added

--- page/NamePage.py
+++ page/NamePage.py
@@ -20,13 +20,13 @@ class NamePage(Page):
                      "nuqneH","Hola","Salut","Hallo","Hey",
                      "Yo","Dia dhuit","Salve","Ciao",u"\u4eca\u65e5\u306f",
                      "Shwmae",u"\u0417\u0434\u0440\u0430\u0432\u0441\u0442\u0432\u0443\u0439\u0442\u0435!"
-                     u"\u4f60\u597d"]
+                     u"\u4f60\u597d", "Booyakasha"]
 
         greeting = choice(greetings)
         #greeting = greetings[-1]
         if "Rafael" in name:
             greeting = "Feo"
-    
+
         if random() < 0.01:
             name = "Jigsaw"
 
