94f1e5a12114cde53c45091b9d6e6e6bf31b8ae6
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-04-30 14:50:48 +0100

    fixed

--- points/points.py
+++ points/points.py
@@ -8,10 +8,10 @@ def update_status(status=None):
 def add_points(house,number):
     with open('/home/pi/.klb/points') as f:
         data = json.load(f)
-    if lines[1] in data:
-        data[lines[1]]+=int(lines[2])
+    if house in data:
+        data[house]+=number
     else:
-        data[lines[1]]=int(lines[2])
+        data[house]=number
     with open('/home/pi/.klb/points','w') as f:
         json.dump(data,f)
-    update_status(status=str(points)+" points to "+house+"!")
+    update_status(status=str(number)+" points to "+house+"!")
