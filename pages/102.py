9ba8af90cbd46ab60c4f16d48a26020eceeb5f26
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-05-14 13:56:16 +0100

    hide past X Men events

--- pages/102.py
+++ pages/102.py
@@ -9,27 +9,34 @@ class XMenPage(Page):
 
     def generate_content(self):
         import urllib2
-    
+        from time import strftime
+
         content = colour_print(printer.text_to_ascii("X-Men Origins Seminar Room"))
     
         response = urllib2.urlopen("https://www.hep.ucl.ac.uk/restricted/mrbs/month.php?area=5&room=15")
         html = response.readlines()
-        y,m,d = ("","","")
+        started = False
+        y,m,d = (int(strftime("%Y")),int(strftime("%m")),int(strftime("%d")))
         for line in html:
             lst = line.strip("\n")
             if '<a href="view_entry.php' in lst:
-                year  = lst.split("year=") [1].split('"')[0]
-                month = lst.split("month=")[1].split("&")[0]
-                day   = lst.split("day=")  [1].split("&")[0]
+                year  = int(lst.split("year=") [1].split('"')[0])
+                month = int(lst.split("month=")[1].split("&")[0])
+                day   = int(lst.split("day=")  [1].split("&")[0])
                 title = lst.split('title="')[1].split('"')[0]
-                if y!=year or m!=month or d!=day:
-                    y=year
-                    m=month
-                    d=day
-                    content += "\n  "+self.colours.Foreground.GREEN
-                    content += y + "-" + m + "-" + "0"*(2-len(d)) + d
-                    content += self.colours.Foreground.DEFAULT + "\n"
-                content += title+"\n"
+                if not started:
+                    if y<=year and m<=month and d<=day:
+                        started = True
+                        y,m,d = (0,0,0)
+                if started:
+                    if y!=year or m!=month or d!=day:
+                            y=year
+                            m=month
+                            d=day
+                            content += "\n  "+self.colours.Foreground.GREEN
+                            content += str(y) + "-" + "0"*(2-len(str(m))) + str(m) + "-" + "0"*(2-len(str(d))) + str(d)
+                            content += self.colours.Foreground.DEFAULT + "\n"
+                    content += title+"\n"
     
         self.content = content
 xmenpage = XMenPage("102")
