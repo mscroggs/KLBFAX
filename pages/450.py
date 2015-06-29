3e12c4b98b57d0916a6dc1c2371b9165974cf8b5
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-06-29 10:03:46 +0100

    awards

--- pages/450.py
+++ pages/450.py
@@ -13,8 +13,11 @@ content = colour_print(
     printer.text_to_ascii("Awards", padding={"left": 6}))
 
 awards = [
-          ["Moo Cow Awards",{"Adam Townsend":3,"Matthew Scroggs":2,"Belgin Seymenoglu":4,"Matthew Wright":2,"Stephen Muirhead":1,"Olly Southwick":2,"Shredder":1,"Pietro Servini":1}],
-          ["Tea Maker Awards",{"Matthew Scroggs":5,"Matthew Wright":5,"Pietro Servini":1,"Peter (who?)":1}],
+          ["Moo Cow Awards",{"Adam Townsend":4,"Matthew Scroggs":2,
+           "Belgin Seymenoglu":5,"Matthew Wright":2,"Stephen Muirhead":1,
+           "Olly Southwick":3,"Shredder":1,"Pietro Servini":2,"Anna Lambert":1}],
+          ["Tea Maker Awards",{"Matthew Scroggs":5,"Matthew Wright":10,"Pietro Servini":1,
+           "Peter (who?)":1,"Olly Southwick":1}],
           ["CelebriTEA",{"Matthew Wright":1,"Matthew Scroggs":1}],
           ["Honorary Fire Marshal",{"Rafael \"Bruce\" Prieto Curiel":1}],
           ["Double Noughts and Crosses",{"Belgin Seymenoglu":1}]
