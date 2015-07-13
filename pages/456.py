062233eece576657a2a0f4b686e8e29f0e93530f
MScroggs <matthew.w.scroggs@gmail.com>
2015-07-13 10:07:20 +0100

    added Boo Cow awards

---
+++ pages/456.py
@@ -0,0 +1,54 @@
+import os
+from page import Page
+from random import choice
+import colours
+from colours import colour_print
+from printer import instance as printer
+
+page_number = os.path.splitext(os.path.basename(__file__))[0]
+sub_page = Page(page_number)
+sub_page.title = "Unawards"
+sub_page.index_num = "456-457"
+content = colour_print(
+    printer.text_to_ascii("Unawards", padding={"left": 6}))
+
+awards = [
+          ["Boo Cow Awards",{"Pietro Servini":1}]
+         ]
+pages = ["457"]
+
+content += "\nWho has the most Unawards?\n\n"
+
+for i,award in enumerate(awards):
+    content += "\n"+sub_page.colours.Foreground.GREEN+award[0]+sub_page.colours.Foreground.DEFAULT+" (see page "+pages[i]+")\n"
+    max_ = 0
+    max_p = None
+    for person,number in award[1].items():
+        if number>max_:
+            max_p = person
+            max_ = number
+        elif number==max_:
+            max_p = max_p+","+person
+    if max_p is not None:
+        content += "  " + max_p + "\n"
+sub_page.content = content
+
+def award_show(award):
+    content = colours.Foreground.GREEN+award[0]+colours.Foreground.DEFAULT+"\n"
+    max_len = 0
+    for person in award[1]:
+        max_len = max(max_len,len(person))
+    for person,number in award[1].items():
+        content += person + (" "*(max_len-len(person)))
+        content += sub_page.colours.Foreground.RED+"|"+sub_page.colours.Foreground.DEFAULT
+        for i in range(number):
+            content += choice(sub_page.colours.Foreground.non_boring)
+            content += u"\u263A"+sub_page.colours.Foreground.DEFAULT
+        content += "\n"
+    return content
+
+def title(text):
+    return colour_print(printer.text_to_ascii(text))
+
+tea_page = Page("457")
+tea_page.content = title("Tea Unwards") + "\n\n" + award_show(awards[0])
