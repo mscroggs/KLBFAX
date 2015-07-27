3f268e5819db8de4ea38cb0ea35edb98b39f46af
MScroggs <matthew.w.scroggs@gmail.com>
2015-07-27 14:06:11 +0100

    (s)

--- pages/103.py
+++ pages/103.py
@@ -7,5 +7,5 @@ page_number = os.path.splitext(os.path.basename(__file__))[0]
 sub_page = Page(page_number)
 sub_page.title = "Important Information"
 sub_page.content = colour_print(
-    printer.text_to_ascii("imperial suck"))
+    printer.text_to_ascii("imperial suck(s)"))
 sub_page.content += "\n\n (Except for Kuru,\n  love from Scroggs)"
