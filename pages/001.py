dd4f744d51f03634acd5c416db3bf377c8d64287
MScroggs <matthew.w.scroggs@gmail.com>
2015-07-28 10:57:50 +0100

    unicode

--- pages/001.py
+++ pages/001.py
@@ -4,24 +4,31 @@ from page import Page
 
 from random import choice
 
-uni_chars = []+range(1000,10000)
+uni_chars = [str(i) for i in []]
+#uni_chars += ["000"+str(i) for i in range(1,10)]
+#uni_chars += ["00"+str(i) for i in range(10,100)]
+#uni_chars += ["0"+str(i) for i in range(100,1000)]
+uni_chars += range(1,10000)
+
 
 class TestPage(Page):
     def __init__(self):
         super(TestPage, self).__init__("001")
         self.in_index = False
         self.title = "Unicode Test Page"
-        self.start = 0
 
     def generate_content(self):
         self.content="\nUNICODE TEST PAGE\n"
         j = 0
+        try:
+            self.start += 0
+        except:
+            self.start = 0
         for i in range(self.start,self.start+200):
             if i<len(uni_chars):
                 ch = uni_chars[i]
             else:
                 ch = choice(uni_chars)
-            uni_chars.remove(ch)
             self.content += str(ch)+" "+unichr(ch)
             j += 1
             if j % 9 != 0: self.content += "  "
