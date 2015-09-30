6cebff4b84337b41340ffa749359584054bac702
MScroggs <matthew.w.scroggs@gmail.com>
2015-09-30 11:24:51 +0100

    fkalhn

--- pages/450.py
+++ pages/450.py
@@ -14,7 +14,7 @@ content = colour_print(
 
 awards = [
           ["Mart Cow Awards",{"Adam Townsend":4,"Matthew Scroggs":2,
-           "Belgin Seymenoglu":13,"Matthew Wright":6,"Stephen Muirhead":3,
+           "Belgin Seymenoglu":13,"Matthew Wright":7,"Stephen Muirhead":3,
            "Olly Southwick":10,"Shredder":1,"Pietro Servini":1,"Anna Lambert":1,
            "Rafael Preito Curiel":1}],
           ["Tea Marter Awards",{"Matthew Scroggs":9,"Matthew Wright":18,"Pietro Servini":1,
@@ -25,7 +25,7 @@ awards = [
           ["Double Noughts and Crosses",{"Belgin Seymenoglu":1}],
           ["Towel Bringer",{"Huda Ramli":2,"Belgin Seymenoglu":1}],
           ["Lunchtime Goat Award",{"Olly Southwick":1}],
-          ["Squeaky Clean",{"Huda Ramli":1,"Rafael \"Bruce\" Prieto Curiel":2,"Belgin Seymenoglu":2}],
+          ["Squeaky Clean",{"Huda Ramli":1,"Rafael \"Bruce\" Prieto Curiel":2,"Belgin Seymenoglu":3}],
           ["Spongebob Squarepoints",{"Rafael Prieto Curiel":1}],
           ["Cleaning the Bloody Fridge",{"Matthew Scroggs":1}]
          ]
