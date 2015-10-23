dbbe9915e88d5638cdd8fc079dd576695cd02f41
MScroggs <matthew.w.scroggs@gmail.com>
2015-10-23 14:54:43 +0100

    COW

--- pages/450.py
+++ pages/450.py
@@ -16,7 +16,7 @@ awards = [
           ["The Award Formerly Known As A Mart Cow Award",{"Adam Townsend":6,"Matthew Scroggs":4,
            "Belgin Seymenoglu":15,"Matthew Wright":7,"Stephen Muirhead":3,
            "Olly Southwick":10,"Shredder":1,"Pietro Servini":1,"Anna Lambert":1,
-           "Rafael Preito Curiel":1}],
+           "Rafael Pietro Curiel":2}],
            # Tea Rex was formerly known as Tea Maker
           ["Tea Rex",{"Matthew Scroggs":10,"Matthew Wright":18,"Pietro Servini":2,
            "Peter (who?)":1,"Olly Southwick":2,"Belgin Seymenoglu":1,
--- pages/456.py
+++ pages/456.py
@@ -13,7 +13,7 @@ content = colour_print(
     printer.text_to_ascii("Unawards", padding={"left": 6}))
 
 awards = [
-          ["Boo Cow Unawards",{"Pietro Servini":1,"Stephen Muirhead":1}],
+          ["Boo Cow Unawards",{"Pietro Servini":1,"Stephen Muirhead":1,"Belgin Seymenoglu":1}],
             # Tea Wrecks was formerly known as Tea Breaker
           ["Tea Wrecks Unawards",{"Anna Lambert":1,"Rafael Prieto Curiel":1,"Belgin Seymenoglu":1}],
           ["Towel Flood Unawards",{"Jigsaw":1}]
