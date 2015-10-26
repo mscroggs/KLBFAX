a42b05dd1bcdef22cdd5cc8a5322566b514a3797
MScroggs <matthew.w.scroggs@gmail.com>
2015-10-26 13:51:30 +0000

    mx->my

--- pages/400.py
+++ pages/400.py
@@ -9,7 +9,7 @@ import time
 #import numpy as np
 from now import now
 from math import cos,sin,floor,pi
-
+from random import choice
 
 class TimePage(Page):
     def __init__(self, page_num):
@@ -37,6 +37,7 @@ class TimePage(Page):
         current_minute = float(now().strftime("%M"))
         current_hour = float(now().strftime("%I"))
         current_hourtopointat = current_hour + current_minute/60.
+
         for a in range(0,num_points+1):
             r = circle_radius*a*.5/num_points
             hx = r*cos(pi/2 - current_hourtopointat*2*pi/12)
@@ -46,14 +47,13 @@ class TimePage(Page):
                     hour_x = screen_radius + int(floor(hx+.5+dx))
                     hour_y = screen_radius - int(floor(hy+.5+dy))
                     hour[hour_y][hour_x] = True
-
-            r = circle_radius*a*.9/num_points
+            r = circle_radius*a*.8/num_points
             mx = r*cos(pi/2 - current_minute*2*pi/60)
             my = r*sin(pi/2 - current_minute*2*pi/60)
             for dx in [-d,d]:
                 for dy in [-d,d]:
                     minute_x = screen_radius + int(floor(mx+.5+dx))
-                    minute_y = screen_radius - int(floor(mx+.5+dy))
+                    minute_y = screen_radius - int(floor(my+.5+dy))
                     minute[minute_y][minute_x] = True
         
 #        hour_x = np.array([r*np.cos(np.pi/2 - current_hourtopointat*2*np.pi/12) for r in np.arange(0,circle_radius*0.5,circle_radius*0.5/num_points)])
