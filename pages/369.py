ee2bc26ecae2409898298d800fe6011b31b360e4
MScroggs <matthew.w.scroggs@gmail.com>
2016-01-04 16:44:02 +0000

    asfhlgjh

--- pages/369.py
+++ pages/369.py
@@ -1,33 +1,31 @@
-import os
-from os.path import join,expanduser
 from page import Page
 from printer import instance as printer
-from math import floor
-from file_handler import f_read_json
-
-def points_format(points,log=0):
-    points = int(points)
-    points /= 10**log
-    points = int(floor(points))
-    return str(points)
+import datetime
 
 class OllyPage(Page):
-    def __init__(self, page_num):
+    def __init__(self, page_num,who,when):
         super(OllyPage, self).__init__(page_num)
-        self.title = "Countdown to Olly Leaving"
+        self.title = "Countdown to "+who+" Leaving"
+        self.who = who
+        self.when = when
 
     def generate_content(self):
-        import datetime
-        delta = datetime.datetime(2016, 3, 24,17,0) - datetime.datetime.now()
-        left = str(delta.days) + " days"
-        left2 = str(delta.seconds/3600) + " hours"
-        content = self.colours.colour_print(printer.text_to_ascii("Olly Leaves In"),foreground=self.colours.Foreground.GREEN+self.colours.Style.BOLD,background=self.colours.Background.BLACK)
+        delta = self.when - datetime.datetime.now()
+
+        hs = delta.seconds/3600
+        ds = delta.days
+
+        left = str(ds) + " day"
+        left2 = str(hs) + " hour"
+        if ds!=1: left += "s"
+        if hs!=1: left2 += "s"
+        content = self.colours.colour_print(printer.text_to_ascii(self.who+" Leaves In"),foreground=self.colours.Foreground.GREEN+self.colours.Style.BOLD,background=self.colours.Background.BLACK)
         content += "\n\n"
         content += self.colours.colour_print(printer.text_to_ascii(left),foreground=self.colours.Foreground.MAGENTA+self.colours.Style.BOLD,background=self.colours.Background.BLACK)
         content += "\n\n"
         content += self.colours.colour_print(printer.text_to_ascii(left2),foreground=self.colours.Foreground.WHITE+self.colours.Style.BOLD,background=self.colours.Background.BLUE)
         self.content = content
 
-page_number = os.path.splitext(os.path.basename(__file__))[0]
-o_page = OllyPage(page_number)
+o_page = OllyPage("369","Olly",datetime.datetime(2016, 3, 24, 17, 0))
+b_page = OllyPage("370","Belgin",datetime.datetime(2018, 8, 31, 16, 0))
 
