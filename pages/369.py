2456384b93d14fbbf294484a600631ac3e8a0f17
MScroggs <matthew.w.scroggs@gmail.com>
2016-01-04 11:59:56 +0000

    afjadgaas olly

--- pages/369.py
+++ pages/369.py
@@ -18,14 +18,14 @@ class OllyPage(Page):
 
     def generate_content(self):
         import datetime
-        delta = datetime.datetime(2016, 03, 24) - datetime.datetime.now()
+        delta = datetime.datetime(2016, 03, 24,17,0) - datetime.datetime.now()
         left = str(delta.days) + " days"
         left2 = str(delta.seconds/3600) + " hours"
-        content = self.colours.colour_print(printer.text_to_ascii("Time til Olly Leaves"),foreground=self.colours.Foreground.GREEN,background=self.colours.Background.RED)
+        content = self.colours.colour_print(printer.text_to_ascii("Olly Leaves In"),foreground=self.colours.Foreground.GREEN+self.colours.Style.BOLD,background=self.colours.Background.BLACK)
         content += "\n\n"
         content += self.colours.colour_print(printer.text_to_ascii(left),foreground=self.colours.Foreground.MAGENTA+self.colours.Style.BOLD,background=self.colours.Background.BLACK)
         content += "\n\n"
-        content += self.colours.colour_print(printer.text_to_ascii(left2),foreground=self.colours.Foreground.WHITE,background=self.colours.Background.BLUE)
+        content += self.colours.colour_print(printer.text_to_ascii(left2),foreground=self.colours.Foreground.WHITE+self.colours.Style.BOLD,background=self.colours.Background.BLUE)
         self.content = content
 
 page_number = os.path.splitext(os.path.basename(__file__))[0]
