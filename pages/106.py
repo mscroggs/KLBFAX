75ab38771c7149dcba9da094e339a18c0144d9c1
MScroggs <matthew.w.scroggs@gmail.com>
2016-01-09 14:51:48 +0000

    more stephen

--- pages/106.py
+++ pages/106.py
@@ -113,13 +113,19 @@ class StephenPage(Page):
                   "Passing vivas"]
         shuffle(skills)
         content = self.colours.colour_print(printer.text_to_ascii("Stephen's Skills"))
+        next = 0
         for skill in skills:
-            content += "\n"
             content += choice(self.colours.Foreground.non_boring)
             content += choice(["",self.colours.Style.BOLD])
             content += skill
             content += self.colours.Foreground.DEFAULT
             content += self.colours.Style.DEFAULT
+            if next == 0:
+                content += " "*max(1,38-len(skill))
+            else:
+                content += "\n"
+            next += 1
+            next %= 2
         self.content = content
 
 
