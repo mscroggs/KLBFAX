58fc896e8a1a1cf0e5dfcd545cbaf1d39aeccf6f
Adam Townsend <pecnut@hotmail.com>
2016-02-11 23:12:47 +0000

    New currencies

--- pages/140.py
+++ pages/140.py
@@ -16,10 +16,11 @@ class CurrencyPage(Page):
         tag = "Live data from Yahoo"
         content = colour_print(printer.text_to_ascii(u"£1 buys"))      
 
-        possible_rates = ['GBPJPY=X','GBPAUD=X','GBPCNY=X','GBPBTC=X','GBPBND=X','GBPMXN=X','GBPCLP=X','GBPETB=X','GBPTRY=X','GBPCHF=X','GBPCAD=X']
-        possible_symbols = [u'¥', 'A$|', u'CN¥|', u'฿', 'B$|', 'MX$|', 'CL$|', 'ETB|', u'₺', 'CHF|', 'C$|']
-        possible_multiple = [1,1,1,1000,1,1,1,1,1,1,1]
-        possible_format = ['{:.0f}','{:.2f}','{:.2f}','{:.2f}','{:.2f}','{:.2f}','{:.0f}','{:.2f}','{:.2f}','{:.2f}','{:.2f}']
+        possible_rates = ['GBPJPY=X','GBPAUD=X','GBPCNY=X','GBPBTC=X','GBPBND=X','GBPMXN=X','GBPCLP=X','GBPETB=X','GBPTRY=X','GBPCHF=X','GBPCAD=X','GBPXAU=X','BZJ16.NYM']
+        possible_symbols_before = [u'¥', 'A$|', u'CN¥|', u'฿', 'B$|', 'MX$|', 'CL$|', 'ETB|', u'₺', 'CHF|', 'C$|', '','']
+        possible_symbols_after = ['','','','','','','','','','','','mg Au','L oil']
+        possible_multiple = [1,1,1,1000,1,1,1,1,1,1,1,31103,1]
+        possible_format = ['{:.0f}','{:.2f}','{:.2f}','{:.2f}','{:.2f}','{:.2f}','{:.0f}','{:.2f}','{:.2f}','{:.2f}','{:.2f}','{:.0f}','{:.2f}']
         
         from random import randint
         random_rate1 = randint(0,len(possible_rates)-1)
@@ -28,10 +29,11 @@ class CurrencyPage(Page):
         #    random_rate2 = randint(0,len(possible_rates)-1)
         
         ask_for_rates = ['GBPUSD=X', 'GBPEUR=X', possible_rates[random_rate1]]#, possible_rates[random_rate2]]
-        currency_symbol = ['$',u"€",possible_symbols[random_rate1]]#,possible_symbols[random_rate2]]
+        currency_symbol_before = ['$',u"€",possible_symbols_before[random_rate1]]#,possible_symbols[random_rate2]]
+        currency_symbol_after = ['','',possible_symbols_after[random_rate1]]#,possible_symbols[random_rate2]]
         currency_multiple = [1,1,possible_multiple[random_rate1]]#,possible_multiple[random_rate2]]
         currency_format = ['{:.2f}','{:.2f}',possible_format[random_rate1]]#,possible_format[random_rate2]]
-        
+               
         currency_rate = []
         req = urllib2.urlopen('http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s='+','.join(ask_for_rates))
         results = req.read()
@@ -39,18 +41,21 @@ class CurrencyPage(Page):
             currency_rate.append(float(results.split(",")[3*i+1]))
         # result = "USDNOK=X",5.9423,"5/3/2010","12:39pm"
 
+        if random_rate1 == 12: #oil
+            currency_rate[2] = 1./currency_rate[2]*158.987*currency_rate[0]
+
         content += "\n\n"
         content += self.colours.colour_print_join([
                         (printer.text_to_ascii("|",False)+"",
                             self.colours.Background.DEFAULT,
                             self.colours.Foreground.BLACK), 
-                        (printer.text_to_ascii(currency_symbol[0] + currency_format[0].format(currency_rate[0]*currency_multiple[0]),False)+"",
+                        (printer.text_to_ascii(currency_symbol_before[0] + currency_format[0].format(currency_rate[0]*currency_multiple[0]),False)+"",
                             self.colours.Background.WHITE,
                             self.colours.Foreground.GREEN),
                         (printer.text_to_ascii("||",False)+"",
                             self.colours.Background.DEFAULT,
                             self.colours.Foreground.BLACK), 
-                        (printer.text_to_ascii(currency_symbol[1] + currency_format[1].format(currency_rate[1]*currency_multiple[1]),False)+"",
+                        (printer.text_to_ascii(currency_symbol_before[1] + currency_format[1].format(currency_rate[1]*currency_multiple[1]),False)+"",
                             self.colours.Background.YELLOW,
                             self.colours.Foreground.BLUE)                          
                     ]," "," ")       
@@ -59,7 +64,7 @@ class CurrencyPage(Page):
                         (printer.text_to_ascii("|",False)+"",
                             self.colours.Background.DEFAULT,
                             self.colours.Foreground.BLACK), 
-                        (printer.text_to_ascii(currency_symbol[2] + currency_format[2].format(currency_rate[2]*currency_multiple[2]),False)+"",
+                        (printer.text_to_ascii(currency_symbol_before[2] + currency_format[2].format(currency_rate[2]*currency_multiple[2]) + currency_symbol_after[2],False)+"",
                             self.colours.Background.WHITE,
                             self.colours.Foreground.BLACK+self.colours.Style.BOLD)#,
                         #(printer.text_to_ascii("||",False)+"",
