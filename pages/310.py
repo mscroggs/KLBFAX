e303af2f035b2d78d244f5103ab2d41b8d1c9820
MScroggs <matthew.w.scroggs@gmail.com>
2016-06-10 13:58:47 +0100

    More soccer

--- pages/310.py
+++ pages/310.py
@@ -18,11 +18,11 @@ def get_team_n(code):
     return None
 
 people = [
-          ["Doaky","Albania","ALB"], ["Rafael","France","FRA"], ["","Romania","ROM"], ["Huda","Switzerland","SWI"],
-          ["Adam","England","ENG"], ["","Russia","RUS"], ["Mart","Slovakia","SVK"], ["Pietro","Wales","WAL"],
-          ["Belgin","Germany","GER"], ["","Northern Ireland","NIR"], ["Grace","Poland","POL"], ["Mattia","Ukraine","UKR"],
-          ["","Croatia","CRO"], ["Anna","Czech Republic","CZE"], ["","Spain","SPA"], ["","Turkey","TUR"],
-          ["Rudie","Belgium","BEL"], ["Luca","Italy","ITA"], ["Sam","Republic of Ireland","ROI"], ["","Sweden","SWE"],
+          ["Doaky","Albania","ALB"], ["Rafael","France","FRA"], ["Jason","Romania","ROM"], ["Huda","Switzerland","SWI"],
+          ["Adam","England","ENG"], ["Sebas","Russia","RUS"], ["Mart","Slovakia","SVK"], ["Pietro","Wales","WAL"],
+          ["Belgin","Germany","GER"], ["Momchil","Northern Ireland","NIR"], ["Grace","Poland","POL"], ["Mattia","Ukraine","UKR"],
+          ["CHARITY","Croatia","CRO"], ["Anna","Czech Republic","CZE"], ["John","Spain","SPA"], ["Newgo","Turkey","TUR"],
+          ["Rudie","Belgium","BEL"], ["Luca","Italy","ITA"], ["Sam","Republic of Ireland","ROI"], ["Antonio","Sweden","SWE"],
           ["Wei Guan","Austria","AUS"], ["Shredder","Hungary","HUN"], ["Scroggs","Iceland","ICE"], ["Olly","Portugal","POR"]
          ]
 
