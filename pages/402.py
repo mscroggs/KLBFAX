c9bc65144fac4b4fd2560c11e3c160b369eb6749
MScroggs <matthew.w.scroggs@gmail.com>
2016-06-10 16:49:19 +0100

    2016ax

--- pages/402.py
+++ pages/402.py
@@ -17,8 +17,9 @@ R = p_page.colours.Foreground.RED
 G = p_page.colours.Foreground.GREEN
 D = p_page.colours.Foreground.DEFAULT
 
-content += G+"Year " + " Gryffindor Slytherin Hufflepuff"    +   " Ravenclaw Durmstrang Squib Peeves"+D+"\n"
-content += G+"2015a"+D+" 692        535       "+R+"1155"+D+"       702       440        513   0\n"
-content += G+"2015b"+D+" 3382       408       "+R+"4614"+D+"       3591      606        2174  38\n"
+content += G+"Year  " + "Gryffindor " + "Slytherin " + "Hufflepuff " + "Ravenclaw " + "Durmstrang " + "Squib " + "Peeves"+D+"\n"
+content += G+"2015a "+D+"692        " + "535       "+R+"1155       "+D+"702       " + "440        " + "513   " + "0\n"
+content += G+"2015b "+D+"3382       " + "408       "+R+"4614       "+D+"3591      " + "606        " + "2174  " + "38\n"
+content += G+"2016a "+D+"1621       " + "378       " + "3640       " + "3407      " + "202        "+R+"3744  "+D+"30\n"
 
 p_page.content = content
