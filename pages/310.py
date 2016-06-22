679b9d1e909b82cf2b750c8d6b7aed5e625db1da
MScroggs <matthew.w.scroggs@gmail.com>
2016-06-22 21:18:32 +0100

    cache the tables

--- pages/310.py
+++ pages/310.py
@@ -109,24 +109,28 @@ class SoccerPage2(Page):
         self.in_index = False
 
     def generate_content(self):
-        # ["ENG","WAL",16,6,14,0,None,None],
-        content = colour_print(printer.text_to_ascii("Euro 2016 Scores & Fixtures")) + "\n"
-        matches = get_groups() + get_knockout()
-        matches.reverse()
-        date = (0,0)
-        nowdate = now()
-        for match in [m for m in matches if m[3]<nowdate.month or (m[3]==nowdate.month and m[2]<=nowdate.day) or (m[2] == 10 and m[3] == 6)]:
-            if date != (match[2],match[3]):
-                date = (match[2],match[3])
-                content +=  " " * 32
-                content += colours.Foreground.YELLOW + colours.Style.BOLD
-                content += str(date[0]) +"/0"+ str(date[1])
-                content += colours.Foreground.DEFAULT + colours.Style.DEFAULT
+        try:
+            # ["ENG","WAL",16,6,14,0,None,None],
+            content = colour_print(printer.text_to_ascii("Euro 2016 Scores & Fixtures")) + "\n"
+            matches = get_groups() + get_knockout()
+            matches.reverse()
+            date = (0,0)
+            nowdate = now()
+            for match in [m for m in matches if m[3]<nowdate.month or (m[3]==nowdate.month and m[2]<=nowdate.day) or (m[2] == 10 and m[3] == 6)]:
+                if date != (match[2],match[3]):
+                    date = (match[2],match[3])
+                    content +=  " " * 32
+                    content += colours.Foreground.YELLOW + colours.Style.BOLD
+                    content += str(date[0]) +"/0"+ str(date[1])
+                    content += colours.Foreground.DEFAULT + colours.Style.DEFAULT
+                    content += "\n"
+                content += write_match(match,40)
                 content += "\n"
-            content += write_match(match,40)
-            content += "\n"
-
-        self.content = content
+    
+            self.content = content
+            self.cache = content
+        except:
+            self.content = self.cache
 
 
 class SoccerPage3(Page):
@@ -136,90 +140,94 @@ class SoccerPage3(Page):
         self.in_index = False
 
     def generate_content(self):
-        # ["ENG","WAL",16,6,14,0,None,None],
-        content = colour_print(printer.text_to_ascii("Euro 2016 Tables"))
-        for p in people:
-            p[3] = [0,0,0,0,0,0]
-        for m in get_groups():
-            n1 = get_team_n(m[0])
-            n2 = get_team_n(m[1])
-            if m[6] is not None:
-                people[n1][3][3] += m[6]
-                people[n1][3][4] += m[7]
-                people[n2][3][4] += m[6]
-                people[n2][3][3] += m[7]
-                if m[6] > m[7]:
-                    people[n1][3][0] += 1
-                    people[n2][3][2] += 1
-                    people[n1][3][5] += 3
-                if m[6] == m[7]:
-                    people[n1][3][1] += 1
-                    people[n2][3][1] += 1
-                    people[n1][3][5] += 1
-                    people[n2][3][5] += 1
-                if m[6] < m[7]:
-                    people[n1][3][2] += 1
-                    people[n2][3][0] += 1
-                    people[n2][3][5] += 3
-        gs = []
-        for i in range(6):
-            group = people[i*4:i*4+4]
-            group.sort(key=lambda p: -p[3][3]+p[3][4])
-            group.sort(key=lambda p: -p[3][5])
-            gs.append(group)
-        for i in range(3):
-            content += "\n"
-            content += colours.Foreground.YELLOW + colours.Style.BOLD
-            content += "Group "+str(i+1)
-            content += " " * 14
-            content += "P W D L F A Pts"
-            content += " " * 4
-            content += "Group "+str(i+4)
-            content += " " * 14
-            content += "P W D L F A Pts"
-            content += colours.Foreground.DEFAULT + colours.Style.DEFAULT
-            content += "\n"
-            for j in range(4):
-                t = gs[i][j]
-                team = (t[1]+" ")[:19-len(t[0])] + colours.Foreground.GREEN + "(" + t[0] + ")" + colours.Foreground.DEFAULT
-                content += team + " "*(30-len(team))
-                pts = ""
-                pts += str(t[3][0]+t[3][1]+t[3][2])
-                pts += " "
-                pts += str(t[3][0])
-                pts += " "
-                pts += str(t[3][1])
-                pts += " "
-                pts += str(t[3][2])
-                pts += " "
-                pts += str(t[3][3])
-                pts += " "
-                pts += str(t[3][4])
-                pts += " "
-                pts += str(t[3][5])
-                content += pts + " "*(19-len(pts))
-
-                t = gs[i+3][j]                
-                team = (t[1]+" ")[:19-len(t[0])] + colours.Foreground.GREEN + "(" + t[0] + ")" + colours.Foreground.DEFAULT
-                content += team + " "*(30-len(team))
-                pts = ""
-                pts += str(t[3][0]+t[3][1]+t[3][2])
-                pts += " "
-                pts += str(t[3][0])
-                pts += " "
-                pts += str(t[3][1])
-                pts += " "
-                pts += str(t[3][2])
-                pts += " "
-                pts += str(t[3][3])
-                pts += " "
-                pts += str(t[3][4])
-                pts += " "
-                pts += str(t[3][5])
-                content += pts
-
+        try:
+            # ["ENG","WAL",16,6,14,0,None,None],
+            content = colour_print(printer.text_to_ascii("Euro 2016 Tables"))
+            for p in people:
+                p[3] = [0,0,0,0,0,0]
+            for m in get_groups():
+                n1 = get_team_n(m[0])
+                n2 = get_team_n(m[1])
+                if m[6] is not None:
+                    people[n1][3][3] += m[6]
+                    people[n1][3][4] += m[7]
+                    people[n2][3][4] += m[6]
+                    people[n2][3][3] += m[7]
+                    if m[6] > m[7]:
+                        people[n1][3][0] += 1
+                        people[n2][3][2] += 1
+                        people[n1][3][5] += 3
+                    if m[6] == m[7]:
+                        people[n1][3][1] += 1
+                        people[n2][3][1] += 1
+                            people[n1][3][5] += 1
+                        people[n2][3][5] += 1
+                    if m[6] < m[7]:
+                        people[n1][3][2] += 1
+                        people[n2][3][0] += 1
+                        people[n2][3][5] += 3
+            gs = []
+            for i in range(6):
+                group = people[i*4:i*4+4]
+                group.sort(key=lambda p: -p[3][3]+p[3][4])
+                group.sort(key=lambda p: -p[3][5])
+                gs.append(group)
+            for i in range(3):
                 content += "\n"
-        self.content = content
+                content += colours.Foreground.YELLOW + colours.Style.BOLD
+                content += "Group "+str(i+1)
+                content += " " * 14
+                content += "P W D L F A Pts"
+                content += " " * 4
+                content += "Group "+str(i+4)
+                content += " " * 14
+                content += "P W D L F A Pts"
+                content += colours.Foreground.DEFAULT + colours.Style.DEFAULT
+                content += "\n"
+                for j in range(4):
+                    t = gs[i][j]
+                    team = (t[1]+" ")[:19-len(t[0])] + colours.Foreground.GREEN + "(" + t[0] + ")" + colours.Foreground.DEFAULT
+                    content += team + " "*(30-len(team))
+                    pts = ""
+                    pts += str(t[3][0]+t[3][1]+t[3][2])
+                    pts += " "
+                    pts += str(t[3][0])
+                    pts += " "
+                    pts += str(t[3][1])
+                    pts += " "
+                    pts += str(t[3][2])
+                    pts += " "
+                    pts += str(t[3][3])
+                    pts += " "
+                    pts += str(t[3][4])
+                    pts += " "
+                    pts += str(t[3][5])
+                    content += pts + " "*(19-len(pts))
+    
+                    t = gs[i+3][j]                
+                    team = (t[1]+" ")[:19-len(t[0])] + colours.Foreground.GREEN + "(" + t[0] + ")" + colours.Foreground.DEFAULT
+                    content += team + " "*(30-len(team))
+                    pts = ""
+                    pts += str(t[3][0]+t[3][1]+t[3][2])
+                    pts += " "
+                    pts += str(t[3][0])
+                    pts += " "
+                    pts += str(t[3][1])
+                    pts += " "
+                    pts += str(t[3][2])
+                    pts += " "
+                    pts += str(t[3][3])
+                    pts += " "
+                    pts += str(t[3][4])
+                    pts += " "
+                    pts += str(t[3][5])
+                    content += pts
+    
+                    content += "\n"
+            self.content = content
+            self.cache = content
+        except:
+            self.content = self.cache
 
 class SoccerPage4(Page):
     def __init__(self, page_num):
@@ -228,41 +236,45 @@ class SoccerPage4(Page):
         self.in_index = False
 
     def generate_content(self):
-        # ["ENG","WAL",16,6,14,0,None,None],
-        import screen
-        content = colour_print(printer.text_to_ascii("Euro 2016 Scores & Fixtures")) + "\n"
-        matches = get_knockout()
-        rounds = {}
-        f = {4:8,5:4,6:2,7:1}
-        for i in [4,5,6,7]:
-            rounds[i] = get_knockout(i)
-            while len(rounds[i]) < f[i]:
-                rounds[i].append(["???","???","??","??","??","??",None,None])
-
-        names = {4:"Second Round",5:"Quarter Finals",6:"Semi Finals",7:"Final"}
-        for i in [4,5,6,7]:
-            content += colours.Foreground.YELLOW + colours.Style.BOLD
-            content += " " * ((screen.WIDTH-len(names[i]))/2) +  names[i]
-            content += colours.Foreground.DEFAULT + colours.Style.DEFAULT
-            content += "\n"
-            next = " "
-            if i==7:
-                wr = write_match(rounds[i][0])
-                content += " " * ((screen.WIDTH+17-len(wr))/2)
-                content += wr
-            else:
-                for match in rounds[i]:
-                    wr = write_match(match,25)
+        try:
+            # ["ENG","WAL",16,6,14,0,None,None],
+            import screen
+            content = colour_print(printer.text_to_ascii("Euro 2016 Scores & Fixtures")) + "\n"
+            matches = get_knockout()
+            rounds = {}
+            f = {4:8,5:4,6:2,7:1}
+            for i in [4,5,6,7]:
+                rounds[i] = get_knockout(i)
+                while len(rounds[i]) < f[i]:
+                    rounds[i].append(["???","???","??","??","??","??",None,None])
+    
+            names = {4:"Second Round",5:"Quarter Finals",6:"Semi Finals",7:"Final"}
+            for i in [4,5,6,7]:
+                content += colours.Foreground.YELLOW + colours.Style.BOLD
+                content += " " * ((screen.WIDTH-len(names[i]))/2) +  names[i]
+                content += colours.Foreground.DEFAULT + colours.Style.DEFAULT
+                content += "\n"
+                next = " "
+                if i==7:
+                    wr = write_match(rounds[i][0])
+                    content += " " * ((screen.WIDTH+17-len(wr))/2)
                     content += wr
-                    if next == " ":
-                        content += " " * (57-len(wr))
-                        next = "\n"
-                    elif next == "\n":
-                        content += "\n"
-                        next = " "
-            content += "\n\n"
-
-        self.content = content
+                else:
+                    for match in rounds[i]:
+                        wr = write_match(match,25)
+                        content += wr
+                        if next == " ":
+                            content += " " * (57-len(wr))
+                            next = "\n"
+                        elif next == "\n":
+                            content += "\n"
+                            next = " "
+                content += "\n\n"
+    
+            self.content = content
+            self.cache = content
+        except:
+            self.content = self.cache
 
 
 class SoccerPage5(Page):
@@ -272,73 +284,77 @@ class SoccerPage5(Page):
         self.in_index = False
 
     def generate_content(self):
-        # ["ENG","WAL",16,6,14,0,None,None],
-        import screen
-        matches = get_groups()
-        for p in people:
-            #      round,for,against,points
-            p[3] = [3,0,0,0]
-        for m in matches:
-            n1 = get_team_n(m[0])
-            n2 = get_team_n(m[1])
-            if m[6] is not None:
-                people[n1][3][1] += m[6]
-                people[n1][3][2] += m[7]
-                people[n2][3][2] += m[6]
-                people[n2][3][1] += m[7]
-                if m[6] > m[7]:
-                    people[n1][3][3] += 3
-                if m[6] == m[7]:
-                    people[n1][3][3] += 1
-                    people[n2][3][3] += 1
-                if m[6] < m[7]:
-                    people[n2][3][3] += 3
-        for i in [4,5,6,7]:
-            matches = get_knockout(i)
+        try:
+            # ["ENG","WAL",16,6,14,0,None,None],
+            import screen
+            matches = get_groups()
+            for p in people:
+                #      round,for,against,points
+                p[3] = [3,0,0,0]
             for m in matches:
                 n1 = get_team_n(m[0])
                 n2 = get_team_n(m[1])
-                people[n1][0] = i
                 if m[6] is not None:
+                    people[n1][3][1] += m[6]
+                    people[n1][3][2] += m[7]
+                    people[n2][3][2] += m[6]
+                    people[n2][3][1] += m[7]
                     if m[6] > m[7]:
-                        people[n1][3][0] = i+1
+                        people[n1][3][3] += 3
+                    if m[6] == m[7]:
+                        people[n1][3][3] += 1
+                        people[n2][3][3] += 1
                     if m[6] < m[7]:
-                        people[n2][3][0] = i+1
-
-        sts = [p for p in people]
-        sts.sort(key=lambda p: p[3][1])
-        sts.sort(key=lambda p: p[3][1]-p[3][2])
-        sts.sort(key=lambda p: p[3][3])
-        sts.sort(key=lambda p: p[3][0])
-
-        content = colour_print(printer.text_to_ascii("Euro 2016 Standings")) + "\n"
-        lines = [p[1] + colours.Foreground.GREEN +" ("+ p[0]+")" + colours.Foreground.DEFAULT for p in sts]
-        content += colours.Foreground.GREEN + "Top" + colours.Foreground.DEFAULT
-        content += " " * 28
-        content += colours.Foreground.RED + "Bottom" + colours.Foreground.DEFAULT 
-        content += "\n"
-
-        n = " ?"
-        for i in range(24):
-            num = ""
-            if i==0 or sts[-i][3] != sts[-i-1][3]:
-                n = ""
-                if i<9:
-                    n += " "
-                n += str(i+1)
-            
-            lines[-1-i] = n + " " + lines[-1-i]
-
-        for i in range(12):
-            t_win = lines[-1-i]
-            t_lose = lines[i]
-            spaces = " " * (41-len(lines[-i-1]))
-            content += t_win
-            content += spaces
-            content += t_lose
+                        people[n2][3][3] += 3
+            for i in [4,5,6,7]:
+                matches = get_knockout(i)
+                for m in matches:
+                    n1 = get_team_n(m[0])
+                    n2 = get_team_n(m[1])
+                    people[n1][0] = i
+                    if m[6] is not None:
+                        if m[6] > m[7]:
+                            people[n1][3][0] = i+1
+                        if m[6] < m[7]:
+                                people[n2][3][0] = i+1
+    
+            sts = [p for p in people]
+            sts.sort(key=lambda p: p[3][1])
+            sts.sort(key=lambda p: p[3][1]-p[3][2])
+            sts.sort(key=lambda p: p[3][3])
+            sts.sort(key=lambda p: p[3][0])
+    
+            content = colour_print(printer.text_to_ascii("Euro 2016 Standings")) + "\n"
+            lines = [p[1] + colours.Foreground.GREEN +" ("+ p[0]+")" + colours.Foreground.DEFAULT for p in sts]
+            content += colours.Foreground.GREEN + "Top" + colours.Foreground.DEFAULT
+            content += " " * 28
+            content += colours.Foreground.RED + "Bottom" + colours.Foreground.DEFAULT 
             content += "\n"
-
-        self.content = content
+    
+            n = " ?"
+            for i in range(24):
+                num = ""
+                if i==0 or sts[-i][3] != sts[-i-1][3]:
+                    n = ""
+                    if i<9:
+                        n += " "
+                    n += str(i+1)
+                
+                lines[-1-i] = n + " " + lines[-1-i]
+    
+            for i in range(12):
+                t_win = lines[-1-i]
+                t_lose = lines[i]
+                spaces = " " * (41-len(lines[-i-1]))
+                content += t_win
+                content += spaces
+                content += t_lose
+                content += "\n"
+    
+            self.content = content
+            self.cache = content
+        except:
+            self.content = self.cache
 
 
 
