c519d9355072e2c083058d20e3c9990575d19004
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-02 21:13:56 +0100

    Fix Adam's crap code

--- pages/119.py
+++ pages/119.py
@@ -1,26 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from re import sub
 from page import Page
-from colours import colour print
+from colours import colour_print
 from printer import instance as printer
 from time import strftime
 import screen
 
 
 class JigPage(Page):
-    def   init  (self,page num):
-        super(JigPage, self).  init  (page num)
-        self.in index = False
+    def __init__(self,page_num):
+        super(JigPage, self).__init__(page_num)
+        self.in_index = False
         self.title = "Squirrel"
         self.tagline = "As found in Belgin's garden"
 
-    def generate content(self):
+    def generate_content(self):
         import random
-		tit = random.choice(["Yupeng","Chunxin"])
+        tit = random.choice(["Yupeng","Chunxin"])
 		
-        content = colour print(printer.text to ascii(tit,fill=True))
+        content = colour_print(printer.text_to_ascii(tit,fill=True,vertical_condense=True))
         content += "\n"
-        content += self.colours.Foreground.YELLOW+"""
-		¶¶¶¶¶¶¶¶¶¶¶
+        content += self.colours.Foreground.YELLOW+u"""
+        ¶¶¶¶¶¶¶¶¶¶¶
      ¶¶¶           ¶¶¶¶¶¶¶
     ¶¶¶¶¶¶¶¶             ¶¶           ¶
       ¶¶¶¶¶¶               ¶¶      ¶¶¶¶  ¶¶
@@ -50,4 +53,4 @@ class JigPage(Page):
 """        
         self.content = content
 
-page = JigPage("117")
+page = JigPage("119")
