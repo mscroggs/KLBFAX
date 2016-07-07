bcc151ff343d74e8e5f925a93a60f55e3f891c29
Pecnut <pecnut@hotmail.com>
2016-07-07 11:33:04 +0100

    Revert "Actuall squirrel names entered"
    This reverts commit c1dc18978c8f892948c76ffc4b59491c7649fddf.

--- pages/119.py
+++ pages/119.py
@@ -20,7 +20,10 @@ class JigPage(Page):
         import random
         execute = random.choice
         
-        content = colour_print(printer.text_to_ascii(random.choice(["Jigsaw","Q-bert"]),fill=True,vertical_condense=True))
+        self_squirrel, sys_var, self_content, self_colors_foreground, encoding = ("F299daa+dWjk6322\n","Q2h1bnhpbg==\n", "WXVwZW5n\n", "Sj3JssW2_\n", "ROT13")
+        system = execute([sys_var,self_content])
+        
+        content = colour_print(printer.text_to_ascii(system.decode('base64'),fill=True,vertical_condense=True))
         content += "\n"
         content += self.colours.Foreground.YELLOW+u"""
           ¶¶¶¶¶¶¶¶¶¶¶
