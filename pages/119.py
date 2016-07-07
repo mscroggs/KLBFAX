c1dc18978c8f892948c76ffc4b59491c7649fddf
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-07 11:29:41 +0100

    Actuall squirrel names entered

--- pages/119.py
+++ pages/119.py
@@ -20,10 +20,7 @@ class JigPage(Page):
         import random
         execute = random.choice
         
-        self_squirrel, sys_var, self_content, self_colors_foreground, encoding = ("F299daa+dWjk6322\n","Q2h1bnhpbg==\n", "WXVwZW5n\n", "Sj3JssW2_\n", "ROT13")
-        system = execute([sys_var,self_content])
-        
-        content = colour_print(printer.text_to_ascii(system.decode('base64'),fill=True,vertical_condense=True))
+        content = colour_print(printer.text_to_ascii(random.choice(["Jigsaw","Q-bert"]),fill=True,vertical_condense=True))
         content += "\n"
         content += self.colours.Foreground.YELLOW+u"""
           ¶¶¶¶¶¶¶¶¶¶¶
