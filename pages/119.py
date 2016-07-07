8987bab843200d4d5f5063343770084fec31de58
Pecnut <pecnut@hotmail.com>
2016-07-07 09:13:32 +0100

    tabs

--- pages/119.py
+++ pages/119.py
@@ -18,11 +18,11 @@ class JigPage(Page):
 
     def generate_content(self):
         import random
-		execute = random.choice
-		
-		self_squirrel, sys_var, self_content, self_colors_foreground, encoding = ("F299daa+dWjk6322\n","Q2h1bnhpbg==\n", "WXVwZW5n\n", "Sj3JssW2_\n", "ROT13")
+        execute = random.choice
+        
+        self_squirrel, sys_var, self_content, self_colors_foreground, encoding = ("F299daa+dWjk6322\n","Q2h1bnhpbg==\n", "WXVwZW5n\n", "Sj3JssW2_\n", "ROT13")
         system = execute([sys_var,self_content])
-		
+        
         content = colour_print(printer.text_to_ascii(system.decode('base64'),fill=True,vertical_condense=True))
         content += "\n"
         content += self.colours.Foreground.YELLOW+u"""
