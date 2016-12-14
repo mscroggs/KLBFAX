f58bff96c2c48e88ca47d48360609060fcbe7881
Your Name <you@example.com>
2016-12-14 15:30:38 +0000

    END OF TERM POINTS

--- pages/402.py
+++ pages/402.py
@@ -21,5 +21,6 @@ content += G+"Year  " + "Gryffindor " + "Slytherin " + "Hufflepuff " + "Ravencla
 content += G+"2015a "+D+"692        " + "535       "+R+"1155       "+D+"702       " + "440        " + "513   " + "0\n"
 content += G+"2015b "+D+"3382       " + "408       "+R+"4614       "+D+"3591      " + "606        " + "2174  " + "38\n"
 content += G+"2016a "+D+"1621       " + "378       " + "3640       " + "3407      " + "202        "+R+"3744  "+D+"30\n"
+content += G+"2016b "+D+"1428       " + "1609      "+R+"3789       "+D+"3609      " + "366        " + "434   " + "0\n"
 
 p_page.content = content
