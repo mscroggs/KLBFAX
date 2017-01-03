28cc37d9725e1ffa8353c3f441be3230baf00682
MScroggs <matthew.w.scroggs@gmail.com>
2017-01-03 14:00:38 +0000

    fix trophy colours

--- pages/113.py
+++ pages/113.py
@@ -29,6 +29,6 @@ class JigPage(Page):
                       333333333333
                      33333333333333
                     3333333333333333
-""", "GREY", "RED", "YELLOW", "BLACK")
+""", "WHITE", "RED", "YELLOW", "GREY")
         
 page = JigPage("113")
