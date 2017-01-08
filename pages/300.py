3414310ae66cc30d0cbc08fa0639082db59a4706
Pecnut <adam@MacPedro.local>
2017-01-08 20:44:21 +0000

    Headline word widths counted better

--- pages/300.py
+++ pages/300.py
@@ -32,24 +32,36 @@ class NewsPage(Page):
         else:
             self.entries = []
 
+
+
+
     def generate_content(self):
+
+        def width_of_word(word):
+            width = len(word)*5 \
+            - sum(map(word.upper().count, u"!:.'I’"))*3
+            - sum(map(word.upper().count, u"-()1"))*2
+            - sum(map(word.upper().count, u"T"))*1
+            + sum(map(word.upper().count, u"MW"))*1
+            return width
+
         import random
         newsreaders = ['Huw Edwards','Lizo from Newsround','Moira Stuart','Nick Owen','Aiming Homes','Michael Burke','Trevor Martdonald','Sam Brown','Mart Pice','Jon Snow','Jeremy Paxperson']
         self.tagline = "Presented by " + random.choice(newsreaders)
 
         self.add_title(self.top_title,bg="BLACK",fg="LIGHTRED")
-
         self.add_newline()
         chars_left = 80
         line = ""
+        self.move_cursor(y=7,x=0)
         for word in self.words:
-            if chars_left - len(word)*5 <= 0:
+            if chars_left - width_of_word(word) <= 0:
                 chars_left = 80
                 self.add_title(line+" ",bg="YELLOW",fg="BLACK",font="size4")
                 line = word + " "
             else:
                 line = line + word + " "
-            chars_left = chars_left - (len(word) + 1)*5
+            chars_left = chars_left - width_of_word(word) - 3
         self.add_title(line+" ",bg="YELLOW",fg="BLACK",font="size4")
         for item in self.entries:
             self.add_text(" - "+ item)
@@ -59,7 +71,7 @@ news_page = NewsPage(300, "http://feeds.bbci.co.uk/news/rss.xml?edition=uk", "Ne
 news_page2 = NewsPage(301, "http://feeds.bbci.co.uk/sport/0/rss.xml?edition=uk", "Sportsmart")
 news_page3 = NewsPage(302, "http://mscroggs.co.uk/blog/rss.xml", "mscroggs.co.ukmart")
 news_page4 = NewsPage(303, "http://www.metoffice.gov.uk/public/data/PWSCache/WarningsRSS/Region/UK", "Weatherwarningmart")
-news_page5 = NewsPage(304, "http://open.live.bbc.co.uk/weather/feeds/en/2643743/3dayforecast.rss", "Forecastmart")
+#news_page5 = NewsPage(304, "http://open.live.bbc.co.uk/weather/feeds/en/2643743/3dayforecast.rss", "Forecastmart")
 news_page6 = NewsPage(305, "http://radiomart.nl/feed/", "Martradiomart")
 news_page7 = NewsPage(306, "https://twitrss.me/twitter_user_to_rss/?user=mathslogicbot", "Truthmart")
 
