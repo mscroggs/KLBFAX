e8a866af206bd86cfa7a8bd47fb62baea021f048
Pecnut <adam@MacPedro.local>
2017-01-19 15:21:59 +0000

    Tweaks

--- pages/107.py
+++ pages/107.py
@@ -20,12 +20,12 @@ class MRoomPage(Page):
             e[1] = parser.parse(e[1])
         self.events = [e for e in self.events if e[1]>now]
 
-        
-      
+
+
     def generate_content(self):
         import config
         from time import strftime
-        
+
         def friendly_date(date):
             if date.date() == datetime.today().date():
                 return "Today"
@@ -37,12 +37,12 @@ class MRoomPage(Page):
         self.add_title("Muirhead Room")
         self.add_newline()
         now = config.now().replace(tzinfo=None)
-    
+
         occupied = False
         for event in self.events:
             if event[0] < now and event[1] > now:
                 occupied = True
-        
+
         if occupied:
             self.start_fg_color("WHITE")
             self.start_bg_color("RED")
@@ -58,22 +58,22 @@ class MRoomPage(Page):
                 next_occupied = self.events[0][0]
             except:
                 next_occupied = datetime.today() + timedelta(days=300)
-            if next_occupied.date() != now.date():             
+            if next_occupied.date() != now.date():
                 message =  "Free all day"
             else:
                 message = "Free until " + next_occupied.strftime("%H:%M")
-            if next_occupied.date() - now.date() <= timedelta(hours=1):             
+            if next_occupied.date() - now.date() <= timedelta(hours=1):
                 self.start_fg_color("BLACK")
                 self.start_bg_color("YELLOW")
             else:
                 self.start_fg_color("BLACK")
                 self.start_bg_color("GREEN")
-            
-        
+
+
         left_banner = " "*int((config.WIDTH - len(message))/2)
         right_banner = " "*int(round((config.WIDTH - len(message))/2))
-        
-              
+
+
         self.add_text(left_banner + message + right_banner)
         self.end_fg_color()
         self.end_bg_color()
@@ -82,7 +82,7 @@ class MRoomPage(Page):
         for event in self.events:
             start_time = event[0]
             end_time = event[1]
-            _name = event[2]
+            _name = event[2].replace("Rafael","Rafeal").replace("Eleanor","Eleanorovirus")
 
             if end_time.date() != previous_date:
                 self.add_newline()
@@ -91,11 +91,11 @@ class MRoomPage(Page):
                 self.end_fg_color()
                 self.add_newline()
             self.start_fg_color("RED")
-            
+
             self.add_text(start_time.strftime("%H:%M") + "-" + end_time.strftime("%H:%M") + " ")
             self.end_fg_color()
             self.add_text(_name)
             self.add_newline()
             previous_date = end_time.date()
-          
+
 muirheadpage = MRoomPage("107")
