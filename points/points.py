37a750a074ef0650674166eed9ac4850acd5b202
602FAX <602@klbfax.co.uk>
2017-02-03 16:28:07 +0000

    make points in 602

--- points/points.py
+++ points/points.py
@@ -1,16 +1,17 @@
 import json
+import config
 from os.path import expanduser, join
 import os
 
 def update_status(status=None):
-  if not os.getenv("SLAVE"):
-    if status is not None:
-        with open(join(expanduser('~'), '.klb/tweet_me'), 'a+') as f:
-            f.write("\n"+status)
+    if config.NAME == "KLBFAX":
+        if status is not None:
+            with open(join(expanduser('~'), '.klb/tweet_me'), 'a+') as f:
+                f.write("\n"+status)
 
 def add_one_random(printing=False):
     from random import choice
-    house = choice(["Gryffindor","Hufflepuff","Slytherin","Ravenclaw","Durmstrang","Squib"])
+    house = choice(["Gryffindor","Hufflepuff","Slytherin","Ravenclaw","Durmstrang","Squib","Postdoc"])
     if printing:
         print("1 point to "+house)
     add_points(house,1)
@@ -46,11 +47,10 @@ def add_points(house, number, deets=""):
 
 
 def should_add_morning_points(time, house, lines, oldname):
-  if not os.getenv("SLAVE"):
+    if config.NAME in ["KLBFAX","602FAX"]:
         if house is not None and "used" not in lines:
             if time in ["08", "09"]:
                 return True
-
         return False
 
 
@@ -66,7 +66,13 @@ def num_of_morning_points(time):
 
 
 def add_morning_points(time, house, oldname, deets):
-    if not os.getenv("SLAVE"):
+    if config.NAME == "KLBFAX":
+        with open("/home/pi/cards/" + oldname, "a") as f:
+            f.write("\nused")
+            points_added = num_of_morning_points(time)
+            add_points(house, points_added, deets)
+        return points_added
+    if config.NAME == "602FAX":
         with open("/home/pi/cards/" + oldname, "a") as f:
             f.write("\nused")
             points_added = num_of_morning_points(time)
