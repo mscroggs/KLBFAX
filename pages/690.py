2026473a1931d72b81e6cb036dc3442cef082d21
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2017-08-21 13:50:09 +0100

    Belgin added recipes

--- pages/690.py
+++ pages/690.py
@@ -44,13 +44,25 @@ Day 10:   Now you are ready to make the cake. Stir well and add the following:
     Mix everything together and put into a large greased baking tin. Sprinkle with a quarter of a cup of brown sugar and a quarter of a cup of melted butter. Bake for 45 minutes at 170-180C. Test the middle with a clean knife; you may need to cover in tin foil and bake for a further 20 minutes to make sure Huda is cooked properly in the middle. When baked, Huda can be frozen.
 """)
 
-r3 = RecipePage("693","Cherry Clafoutis","""
-    4       Eggs
-    1 jar   Cherries
-    ? ???   Flour
+r3 = RecipePage("693","Kvass",
+"""
+    2 L     Water
+    100g    Sugar
+    7g      Yeast
+    6 slices of Bread
 
+1. Toast bread until super-brown (or burnt!)
+2. Boil the water in a kettle
+3. Put the burnt bread and boiled water in a pan.
+4. Leave for 5-10 minutes ot until the water is tea-coloured.
+5. Take the bread out the pan and put in a sieve above the pan. Squeeze out any water from the soggy bread.
+6. Discard the bread.
+7. Stir in the sugar, then the yeast.
+8. Transfer to a glass bottle and leave out in the sun for half a day.
 
-1. To be continued...""")
+Note: The longer you leave the kvass out, the better. Three days is perfect!
+
+""")
 
 r4 = RecipePage("694","Pesto Twists",
 """
@@ -82,6 +94,39 @@ r5 = RecipePage("695","Pesto",
 
     Note: You can make other kinds of pesto if you replace the basil with another ingredient, such as rocket, mint or sundried tomatoes. In fact, if you put straw rather than basil leaves, you end up with HEY PESTO!""")
 
+r6 = RecipePage("696","Chalkdust Rocky Road","""
+INGREDIENTS
+
+    300g    Marshmallows
+    300g    Chocolate
+    100g    Butter
+    80g     Youghurt + cream
+    50g     Hazelnuts
+    50g     Walnuts
+    50g     Pistachio nuts
+    50g     Blanched almonds
+    50g     Sunflower seeds
+    
+    Turn to Page 697 for the method.
+""")
+
+r7 = RecipePage("697","Chalkdust Rocky Road","""
+
+METHOD
+
+1. Toast all nuts except the walnuts.
+2. Melt chocolate in pan over a medium heat.
+3. Add marshmallows and butter, stirring occasionally.
+4. Wait until everything has melted.
+5. Add nuts and mix well.
+6. Spoon mixture onto a buttered tray.
+7. Leave in the fridge overnight.
+8. The next morning, cut up the cake into squares.
+
+Turn to Page 696 for the ingredients.
+
+Note: You can also add maple syrup!
+""")
 
 class RecipeIPage(Page):
     def __init__(self):
@@ -90,7 +135,7 @@ class RecipeIPage(Page):
 
     def generate_content(self):
         self.add_title("Recipes")
-        for r in [r1,r2,r3,r4,r5]:
+        for r in [r1,r2,r3,r4,r5,r6,r7]:
             self.add_text(r.number,bg="BLUE")
             self.add_text(" "+r.title)
             self.add_newline()
