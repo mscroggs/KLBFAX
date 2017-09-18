5bd7855f237f7b73e3b87dad6b6377c2ebe6e745
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2017-09-18 22:14:10 +0100

    OXMAS!

---
+++ pages/710.py
@@ -0,0 +1,25 @@
+import os
+from page import Page
+from ceefax import Ceefax
+from random import shuffle
+from config import NAME
+from file_handler import f_read_json
+from datetime import datetime
+
+class OxmasPage(Page):
+    def __init__(self, n):
+        super(OxmasPage, self).__init__(n)
+        self.title = "Oxmas Attendees"
+        if NAME!="602FAX":
+            self.is_enabled = False
+            self.in_index = False
+
+    def generate_content(self):
+        self.add_title("OXMAS")
+        db = f_read_json("db.json")
+        for person in db:
+            if person["coming"] == "yes":
+                self.add_text(person["name"][1] + " (" + person["name"][0] + ")")
+                self.add_newline()
+
+i_p = OxmasPage("710")
