77c59e92a36879d588dc43becabd6c84591ce36f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2017-09-19 07:18:50 +0100

    omasxo

--- pages/710.py
+++ pages/710.py
@@ -15,15 +15,20 @@ class OxmasPage(Page):
             self.in_index = False
 
     def generate_content(self):
-        self.add_title("OXMAS")
+        self.add_title("OXMAS", foreground="GREEN",background="BLACK")
         db = f_read_json("db.json")
         i = 0
         for person in db:
             if person["coming"] == "yes":
                 self.add_text(person["name"][1] + " (" + person["name"][0] + ")")
+                if person["santa"] == "yes":
+                    self.start_fg_color("RED")
+                    self.add_text(" S")
+                    self.end_fg_color()
                 if i%2==0:
                     self.move_cursor(x=35)
                 else:
                     self.add_newline()
+                i += 1
 
 i_p = OxmasPage("710")
