daeab5fa13c9675bb1de6b5147a852cf87e57411
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2017-09-19 07:35:45 +0100

    oxmart!!!

--- pages/710.py
+++ pages/710.py
@@ -7,28 +7,78 @@ from file_handler import f_read_json
 from datetime import datetime
 
 class OxmasPage(Page):
-    def __init__(self, n):
+    def __init__(self, n, title, test):
         super(OxmasPage, self).__init__(n)
-        self.title = "Oxmas Attendees"
+        self.title = "Oxmas "+title
+        self.in_index = False
+        self.test = test
+        self._name = title
         if NAME!="602FAX":
             self.is_enabled = False
-            self.in_index = False
 
     def generate_content(self):
-        self.add_title("OXMAS", fg="GREEN",bg="BLACK")
+        self.add_title("OXMAS "+self._name, fg="GREEN",bg="BLACK")
         db = f_read_json("db.json")
         i = 0
         for person in db:
-            if person["coming"] == "yes":
+            if self.test(person):
                 self.add_text(person["name"][1] + " (" + person["name"][0] + ")")
-                if person["santa"] == "yes":
-                    self.start_fg_color("RED")
-                    self.add_text(" S")
-                    self.end_fg_color()
                 if i%2==0:
                     self.move_cursor(x=35)
                 else:
                     self.add_newline()
                 i += 1
 
-i_p = OxmasPage("710")
+class OxmasMenu(Page):
+    def __init__(self, n):
+        super(OxmasMenu, self).__init__(n)
+        self.title = "Oxmas Menu"
+        self.in_index = False
+        if NAME!="602FAX":
+            self.is_enabled = False
+
+    def generate_content(self):
+        self.add_title("OXMAS Menu", fg="GREEN",bg="BLACK")
+        self.add_newline()
+        self.add_text("Meat: ",fg="RED")
+        self.add_wrapped_text("Chicken, Lamb, Ham, Pork, Beef, Sausages wrapped in Bacon")
+        self.add_newline()
+        self.add_newline()
+
+        self.add_text("Vegetarian: ",fg="GREEN")
+        self.add_wrapped_text("Amazing bread thingy")
+        self.add_newline()
+        self.add_newline()
+
+        self.add_text("On the side: ",fg="RED")
+        self.add_wrapped_text("Roast Potatoes, Parsnips, Stuffing, Peas, Carrots, Leeks, Broccoli, Cauliflower, Brussels Sprouts")
+        self.add_newline()
+        self.add_newline()
+
+        self.add_text("Sauces: ",fg="GREEN")
+        self.add_wrapped_text("Meaty Gravy, Veggie Gravy, Mint Sauce, Cranberry Sauce")
+        self.add_newline()
+        self.add_newline()
+
+        self.add_text("Puddings: ",fg="RED")
+        self.add_wrapped_text("Tipsy Cake, Bad-TempeGREEN Cake (Tiffin), Eton Mess, Brandy Butter, Oxmas Cake, Tiramisu, Cheese Cake, Mince Pies")
+        self.add_newline()
+        self.add_newline()
+
+        self.add_text("Cheese board: ",fg="RED")
+        self.add_wrapped_text("Cheddar, Stilton, Brie")
+        self.add_newline()
+        self.add_newline()
+
+        self.add_text("Drinks: ",fg="GREEN")
+        self.add_wrapped_text("Mulleed Wine, Selected Soft Drinks")
+        self.add_newline()
+        self.add_newline()
+
+
+attendees = OxmasPage("710","Attendees",lambda person:person["coming"]=="yes")
+santas = OxmasPage("711","Santas",lambda person:person["santa"]=="yes")
+menu = OxmasMenu("712")
+if NAME=="602FAX":
+    attendees.title="OXMAS!"
+    attendees.index_num="710-712"
