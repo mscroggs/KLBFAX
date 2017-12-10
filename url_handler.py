dca69d76ea70e5dc5cb2fd9b4a9b71a20c251636
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2017-12-10 09:38:33 +0000

    more useful errors please

--- url_handler.py
+++ url_handler.py
@@ -7,13 +7,18 @@ def load_json(url):
 def load(url):
     try:
         import urllib2
-        response = urllib2.urlopen(url)
-        return response.read()
+        v = 2
     except:
         import urllib.request
+        v = 3
+    if v == 2:
+        response = urllib2.urlopen(url)
+        return response.read()
+    if v == 3:
         response = urllib.request.urlopen(url)
         out = response.read()
         try:
             return out.decode(response.info().get_content_charset('utf-8'))
         except:
             return out
+
