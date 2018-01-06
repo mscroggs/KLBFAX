2f4b857a3d9ed0caae2602accda46991b746fd76
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-01-06 09:18:11 +0000

    Give licence to all contributors, not just me

--- pages/003.py
+++ pages/003.py
@@ -7,11 +7,16 @@ class LiPage(Page):
         self.is_enabled = False
         self.title = "License"
 
+
     def generate_content(self):
         from random import randrange
         self.add_title("License",font="size4")
         from file_handler import load_file
         li = load_file("../LICENSE.txt")
+        c = load_file("../contributors.txt").split("\n")
+        while "" in c:
+            c.remove("")
+        li = li.replace("The KLBFAX contributors",", ".join(c))
         self.add_wrapped_text(li)
 
 sub_page = LiPage()
