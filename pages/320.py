5de92a5a52563bbfcb54e6f0553486a21c5b4a10
Pecnut <pecnut@hotmail.com>
2018-01-07 16:59:01 +0000

    weather fix 2

--- pages/320.py
+++ pages/320.py
@@ -15,7 +15,8 @@ class WeatherPage(Page):
             self.weather_data = response.split(" ")
         except:
             self.weather_data = ["CLASSIFIED"]*60
-        ''' If using realtime.txt (no longer working on CASA website):
+        ''' 
+        If using realtime.txt (no longer working on CASA website):
         # Field # Example 	Description 	Equivalent Webtags
         # 1 	19/08/09 	Date as 2 figure day [separator] 2 figure month [separator] 2 figure year - the separator is that set in the windows system short date format (see setup) 	<#date>
         # 2 	16:03:45 	time(always hh:mm:ss as per computer system) 	<#timehhmmss>
@@ -75,15 +76,15 @@ class WeatherPage(Page):
         # 56 	11.1 	Sunshine hours so far today 	<#SunshineHours>
         # 57 	420.1 	Current theoretical max solar radiation 	<#CurrentSolarMax>
         # 58 	1 	Is it sunny? 1 if the sun is shining, otherwise 0 (above or below threshold) 	<#IsSunny>
-        '''
+        
         # If using clientraw.txt:
-        '''
+        
         Field#  Label   Type    Contents
         000     Header  L   12345
         001     Avg Speed   K   7.6kts = 8.75mph = 14.08km/h
         002     Gusts   K   5.2kts = 5.98mph = 9.63km/h
-        003     WindDir     D   320° (NW)
-        004     Temp    C   5.3°C = 41.5°F
+        003     WindDir     D   320* (NW)
+        004     Temp    C   5.3C = 41.5F
         005     Outside Humidity    P   64%
         006     Barometer   H   1024.0 hPa = 30.24 inHg
         007     Daily Rain  M   0.0mm = 0in
@@ -91,15 +92,15 @@ class WeatherPage(Page):
         009     Yearly Rain     M   25.7mm = 1.01in
         010     Rain Rate   M   0.00mm = 0in
         011     Max Rain Rate   M   0.00mm = 0in
-        012     Indoor Temp     C   23.2°C = 73.8°F
+        012     Indoor Temp     C   23.2*C = 73.8*F
         013     Indoor Hum  P   26%
-        014     Soil Temp   C   100.0°C = 212°F
+        014     Soil Temp   C   100.0*C = 212*F
         015     Forecast Icon   I   5 (Dry Clear)
-        016     WMR968 Extra Temp   C   0.0°C = 32°F
+        016     WMR968 Extra Temp   C   0.0*C = 32*F
         017     WMR968 Extra Hum    P   0%
         018     WMR968 Extra Sensor     N   0
         019     Yesterday Rain  M   0.0mm = 0in
-        020     Extra Temp Sensor 1     C   12.8°C = 55°F
+        020     Extra Temp Sensor 1     C   12.8*C = 55*F
         021     Extra Temp Sensor 2     C   No Value
         022     Extra Temp Sensor 3     C   No Value
         023     Extra Temp Sensor 4     C   No Value
@@ -123,10 +124,10 @@ class WeatherPage(Page):
         041     WMR968 batt5    P   100%
         042     WMR968 batt6    P   100%
         043     WMR968 batt7    P   100%
-        044     WindChill   C   3.1°C = 37.6°F
-        045     Humidex     C   2.9°C = 37.2°F
-        046     Max Day Temp    C   7.1°C = 44.8°F
-        047     Min Day Temp    C   3.8°C = 38.8°F
+        044     WindChill   C   3.1*C = 37.6*F
+        045     Humidex     C   2.9*C = 37.2*F
+        046     Max Day Temp    C   7.1*C = 44.8*F
+        047     Min Day Temp    C   3.8*C = 38.8*F
         048     Icon Type   Z   5 (Dry Clear)
         049     Weather Desc    L   dusk/Dry
         050     Baro Trend  H   0.3 hPa = 0.01 inHg
@@ -151,13 +152,13 @@ class WeatherPage(Page):
         069     Windspeed Hour 19   K   8kts = 9.21mph = 14.82km/h
         070     Windspeed Hour 20   K   5kts = 5.75mph = 9.26km/h
         071     Max Wind Gust Today     K   21.0kts = 24.17mph = 38.89km/h
-        072     DewPoint Temp   C   -0.9°C = 30.4°F
+        072     DewPoint Temp   C   -0.9*C = 30.4*F
         073     Cloud Height    F   824 Meters - 2703.5Feet
         074     Date    L   1/7/2018
-        075     Max Humidex     C   4.9°C = 40.8°F
-        076     Min Humidex     C   1.6°C = 34.9°F
-        077     Max Windchill   C   7.1°C = 44.8°F
-        078     Min Windchill   C   -1.4°C = 29.5°F
+        075     Max Humidex     C   4.9*C = 40.8*F
+        076     Min Humidex     C   1.6*C = 34.9*F
+        077     Max Windchill   C   7.1*C = 44.8*F
+        078     Min Windchill   C   -1.4*C = 29.5*F
         079     Davis VP UV     N   0.0
         080     Hr Windspeed 01     K   5kts = 5.75mph = 9.26km/h
         081     Hr Windspeed 02     K   6kts = 6.9mph = 11.11km/h
@@ -169,16 +170,16 @@ class WeatherPage(Page):
         087     Hr Windspeed 08     K   6kts = 6.9mph = 11.11km/h
         088     Hr Windspeed 09     K   6kts = 6.9mph = 11.11km/h
         089     Hr Windspeed 10     K   6kts = 6.9mph = 11.11km/h
-        090     Hr Temp 01  C   6.0°C = 42.8°F
-        091     Hr Temp 02  C   5.9°C = 42.6°F
-        092     Hr Temp 03  C   6.0°C = 42.8°F
-        093     Hr Temp 04  C   5.8°C = 42.4°F
-        094     Hr Temp 05  C   5.8°C = 42.4°F
-        095     Hr Temp 06  C   5.7°C = 42.3°F
-        096     Hr Temp 07  C   5.6°C = 42.1°F
-        097     Hr Temp 08  C   5.5°C = 41.9°F
-        098     Hr Temp 09  C   5.4°C = 41.7°F
-        099     Hr Temp 10  C   5.3°C = 41.5°F
+        090     Hr Temp 01  C   6.0*C = 42.8*F
+        091     Hr Temp 02  C   5.9*C = 42.6*F
+        092     Hr Temp 03  C   6.0*C = 42.8*F
+        093     Hr Temp 04  C   5.8*C = 42.4*F
+        094     Hr Temp 05  C   5.8*C = 42.4*F
+        095     Hr Temp 06  C   5.7*C = 42.3*F
+        096     Hr Temp 07  C   5.6*C = 42.1*F
+        097     Hr Temp 08  C   5.5*C = 41.9*F
+        098     Hr Temp 09  C   5.4*C = 41.7*F
+        099     Hr Temp 10  C   5.3*C = 41.5*F
         100     Hr Rain 01  M   0.0mm = 0in
         101     Hr Rain 02  M   0.0mm = 0in
         102     Hr Rain 03  M   0.0mm = 0in
@@ -189,16 +190,16 @@ class WeatherPage(Page):
         107     Hr Rain 08  M   0.0mm = 0in
         108     Hr Rain 09  M   0.0mm = 0in
         109     Hr Rain 10  M   0.0mm = 0in
-        110     Max Heat Index  C   7.1°C = 44.8°F
-        111     Min Heat Index  C   3.8°C = 38.8°F
-        112     Heat Index  C   5.3°C = 41.5°F
+        110     Max Heat Index  C   7.1*C = 44.8*F
+        111     Min Heat Index  C   3.8*C = 38.8*F
+        112     Heat Index  C   5.3*C = 41.5*F
         113     Max Avg Speed   K   13.1kts = 15.08mph = 24.26km/h
         114     # Lightning in last Min     N   0
         115     Time Last Lightning Strike  L   ---
         116     Date Last Lightning Strike  L   ---
-        117     Wind Avg Dir    D   3° (N)
+        117     Wind Avg Dir    D   3* (N)
         118     Nexstorm Dist   L   0
-        119     NexStorm Bearing    D   0° (N)
+        119     NexStorm Bearing    D   0* (N)
         120     Extra Temp Sensor 7     C   No Value
         121     Extra Temp Sensor 8     C   No Value
         122     Extra Hum Sensor 4  P   0%
@@ -207,38 +208,38 @@ class WeatherPage(Page):
         125     Extra Hum Sensor 7  P   No Value
         126     Extra Hum Sensor 8  P   No Value
         127     VP Solarwm  N   0.0
-        128     Max InTemp  C   23.7°C = 74.7°F
-        129     Min InTemp  C   22.9°C = 73.2°F
-        130     Apparent Temp   C   1.4°C = 34.5°F
+        128     Max InTemp  C   23.7*C = 74.7*F
+        129     Min InTemp  C   22.9*C = 73.2*F
+        130     Apparent Temp   C   1.4*C = 34.5*F
         131     Max Baro    H   1024.6 hPa = 30.26 inHg
         132     Min Baro    H   1019.0 hPa = 30.09 inHg
         133     Max Gust Last Hour  K   17kts = 19.56mph = 31.48km/h
         134     Max Gust Last Hour Time     L   3:51PM
         135     Max Gust Today Time     L   1:14 PM
-        136     Max Apparent Temp   C   4.9°C = 40.8°F
-        137     Min Apparent Temp   C   -0.3°C = 31.5°F
-        138     Max Dew Pt  C   2.2°C = 36°F
-        139     Min Dew Pt  C   -0.9°C = 30.4°F
+        136     Max Apparent Temp   C   4.9*C = 40.8*F
+        137     Min Apparent Temp   C   -0.3*C = 31.5*F
+        138     Max Dew Pt  C   2.2*C = 36*F
+        139     Min Dew Pt  C   -0.9*C = 30.4*F
         140     Max Gust In Lst Min     K   13kts = 14.96mph = 24.08km/h
         141     Current Year    L   2018
         142     THSWS   L   -17.8
         143     Temp Trend (Logic)  L   -1
         144     Humidity Trend (Logic)  L   -1
         145     Humidex Trend (Logic)   L   -1
-        146     Hr Wind Dir 01  D   317° (NW)
-        147     Hr Wind Dir 02  D   340° (NNW)
-        148     Hr Wind Dir 03  D   300° (WNW)
-        149     Hr Wind Dir 04  D   304° (WNW)
-        150     Hr Wind Dir 05  D   19° (N)
-        151     Hr Wind Dir 06  D   355° (NNW)
-        152     Hr Wind Dir 07  D   288° (W)
-        153     Hr Wind Dir 08  D   89° (ENE)
-        154     Hr Wind Dir 09  D   306° (WNW)
-        155     Hr Wind Dir 10  D   320° (NW)
-        156     Leaf Wetness    C   0.0°C = 32°F
+        146     Hr Wind Dir 01  D   317* (NW)
+        147     Hr Wind Dir 02  D   340* (NNW)
+        148     Hr Wind Dir 03  D   300* (WNW)
+        149     Hr Wind Dir 04  D   304* (WNW)
+        150     Hr Wind Dir 05  D   19* (N)
+        151     Hr Wind Dir 06  D   355* (NNW)
+        152     Hr Wind Dir 07  D   288* (W)
+        153     Hr Wind Dir 08  D   89* (ENE)
+        154     Hr Wind Dir 09  D   306* (WNW)
+        155     Hr Wind Dir 10  D   320* (NW)
+        156     Leaf Wetness    C   0.0*C = 32*F
         157     Soil moisture   C   No Value
         158     10 Min Avg Wind Speed   K   5.9kts = 6.79mph = 10.93km/h
-        159     Wet bulb temperature    C   3.0°C = 37.4°F
+        159     Wet bulb temperature    C   3.0*C = 37.4*F
         160     Latitude (- for southern hemisphere)    T   51.52167
         161     Longitude (- for EAST of GMT)   T   0.13500
         162     9am Rain Reset Total    M   0.0mm = 0in
@@ -255,10 +256,12 @@ class WeatherPage(Page):
         173     Daily Wind Run 9am/Mid Reset    K   155.5kts = 178.95mph = 287.99km/h
         174     Record End (WD Ver)     L   1:09_PM
         '''
+
     def generate_content(self):
+
         weather_date_list = self.weather_data[74].split("/") # It's in US format by default
-        weather_date = weather_date_list[1] + "/" + weather_date_list[0] + "/" + weather_date_list[2]
-        weather_time = self.weather_data[29] + ":" + self.weather_data[30] + ":" + self.weather_data[31]
+        weather_date = str(weather_date_list[1]) + "/" + str(weather_date_list[0]) + "/" + str(weather_date_list[2])
+        weather_time = str(self.weather_data[29]) + ":" + str(self.weather_data[30]) + ":" + str(self.weather_data[31])
         weather_temperature = self.weather_data[4]
         weather_humidity = self.weather_data[5]
         weather_wind_speed = self.weather_data[1]
