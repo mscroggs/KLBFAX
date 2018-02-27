47e4b7f9ed7bb94f5d4c1450444c2a44aad49159
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-02-27 17:43:15 +0000

    striek

--- page/special/StrikePage.py
+++ page/special/StrikePage.py
@@ -1,122 +1,124 @@
 from page import Page
 import config
 from random import choice, randint
-strike = ""
-strike += "----wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww-\n"
-strike += "----wrrrrrrwrrrrrrwrrrrrwwrrrrrrwrrwwrrwrrrrrrw-\n"
-strike += "----wrrwwwwwwwrrwwwrrwwrrwwwrrwwwrrwrrwwrrwwwww-\n"
-strike += "----wrrrrrrwwwrrwwwrrrrwwwwwrrwwwrrrrwwwrrrrwww-\n"
-strike += "----wwwwwrrwwwrrwwwrrwrrwwwwrrwwwrrwrrwwrrwwwww-\n"
-strike += "----wrrrrrrwwwrrwwwrrwwrrwrrrrrrwrrwwrrwrrrrrrw-\n"
-strike += "----wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww-\n"
-strike += "-------------------------g----------------------\n"
-strike += "-------------------------g----------------------\n"
-strike += "-------------------------g------000000----------\n"
-strike += "-------------------------g------000000----------\n"
-strike += "-------------------------g----0000000000--------\n"
-strike += "-------------------------g----0000000000--------\n"
-strike += "-------------------------g----0000000000--------\n"
-strike += "-------------------------g----0000000000--------\n"
-strike += "-------------------------g----0000000000--------\n"
-strike += "-------------------------g----0000000000--------\n"
-strike += "-------------------------g------000000----------\n"
-strike += "-------------------------g------000000----------\n"
-strike += "-------------------------g--------00------------\n"
-strike += "-------------------------g--------00------------\n"
-strike += "-------------------------0000000000000----------\n"
-strike += "-------------------------0000000000000----------\n"
-strike += "----------------------------------00--00--------\n"
-strike += "----------------------------------00--00--------\n"
-strike += "----------------------------------00--00--------\n"
-strike += "----------------------------------00--00--------\n"
-strike += "----------------------------------00--00--------\n"
-strike += "----------------------------------00--00--------\n"
-strike += "----------------------------------00------------\n"
-strike += "----------------------------------00------------\n"
-strike += "----------------------------------00------------\n"
-strike += "----------------------------------00------------\n"
-strike += "--------------------------------000000----------\n"
-strike += "--------------------------------000000----------\n"
-strike += "--------------------------------00--00----------\n"
-strike += "--------------------------------00--00----------\n"
-strike += "--------------------------------00--00----------\n"
-strike += "--------------------------------00--00----------\n"
-strike += "--------------------------------00--00----------\n"
-strike += "--------------------------------00--00----------\n"
-strike += "------------------------------0000--0000--------\n"
-strike += "------------------------------0000--0000--------"
 
-strike2 = ""
-strike2 += "----wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww-\n"
-strike2 += "----wrrrrrrwrrrrrrwrrrrrwwrrrrrrwrrwwrrwrrrrrrw-\n"
-strike2 += "----wrrwwwwwwwrrwwwrrwwrrwwwrrwwwrrwrrwwrrwwwww-\n"
-strike2 += "----wrrrrrrwwwrrwwwrrrrwwwwwrrwwwrrrrwwwrrrrwww-\n"
-strike2 += "----wwwwwrrwwwrrwwwrrwrrwwwwrrwwwrrwrrwwrrwwwww-\n"
-strike2 += "----wrrrrrrwwwrrwwwrrwwrrwrrrrrrwrrwwrrwrrrrrrw-\n"
-strike2 += "----wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww-\n"
-strike2 += "-------------------------g----------------------\n"
-strike2 += "-------------------------g----------------------\n"
-strike2 += "-------------000000------g----------------------\n"
-strike2 += "-------------000000------g----------------------\n"
-strike2 += "-----------0000000000----g----------------------\n"
-strike2 += "-----------0000000000----g----------------------\n"
-strike2 += "-----------0000000000----g----------------------\n"
-strike2 += "-----------0000000000----g----------------------\n"
-strike2 += "-----------0000000000----g----------------------\n"
-strike2 += "-----------0000000000----g----------------------\n"
-strike2 += "-------------000000------g----------------------\n"
-strike2 += "--------------00000------g----------------------\n"
-strike2 += "---------------00--------g----------------------\n"
-strike2 += "---------------00--------g----------------------\n"
-strike2 += "-------------0000000000000----------------------\n"
-strike2 += "-------------0000000000000----------------------\n"
-strike2 += "-----------00--00-------------------------------\n"
-strike2 += "-----------00--00-------------------------------\n"
-strike2 += "-----------00--00-------------------------------\n"
-strike2 += "-----------00--00-------------------------------\n"
-strike2 += "-----------00--00-------------------------------\n"
-strike2 += "---------------00-------------------------------\n"
-strike2 += "---------------00-------------------------------\n"
-strike2 += "---------------00-------------------------------\n"
-strike2 += "---------------00-------------------------------\n"
-strike2 += "-------------000000-----------------------------\n"
-strike2 += "-------------000000-----------------------------\n"
-strike2 += "-------------00--00-----------------------------\n"
-strike2 += "-------------00--00-----------------------------\n"
-strike2 += "-------------00--00-----------------------------\n"
-strike2 += "-------------00--00-----------------------------\n"
-strike2 += "-------------00--00-----------------------------\n"
-strike2 += "-------------00--00-----------------------------\n"
-strike2 += "-----------0000--0000---------------------------\n"
-strike2 += "-----------0000--0000---------------------------"
+def join(a,b):
+    return "\n".join([i+j for i,j in zip(a.split("\n"),b.split("\n"))])
 
-mike  = "----wwwwwwwwwwwwwrrwwwwrrwrrrrrrwrrwwrrwrrrrrrw-\n"
-mike += "----wwwwwwwwwwwwwrrrwwrrrwwwrrwwwrrwrrwwrrwwwww-\n"
-mike += "----wwwwwwwwwwwwwrrwrrwrrwwwrrwwwrrrrwwwrrrrwww-\n"
-mike += "----wwwwwwwwwwwwwrrwwwwrrwwwrrwwwrrwrrwwrrwwwww-\n"
-mike += "----wwwwwwwwwwwwwrrwwwwrrwrrrrrrwrrwwrrwrrrrrrw-\n"
+def make_strike_one():
+    r = randint(0,40)
+    strike = ""
+    strike += "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww-\n"
 
-boards  = "----.rrrrr...rrrr...rrrr..rrrrr..rrrrr...rrrrr.-\n"
-boards += "----.rr..rr.rr..rr.rr..rr.rr..rr.rr..rr.rr.....-\n"
-boards += "----.rrrrr..rr..rr.rrrrrr.rrrrrr.rr..rr..rrrr..-\n"
-boards += "----.rr..rr.rr..rr.rr..rr.rr.rr..rr..rr.....rr.-\n"
-boards += "----.rrrrr...rrrr..rr..rr.rr..rr.rrrrr..rrrrr..-\n"
-boards = boards.replace(".","w")
+    if r==0:
+        strike += "wwwwwwwwwwwwwrrwwwwrrwrrrrrrwrrwwrrwrrrrrrw-\n"
+        strike += "wwwwwwwwwwwwwrrrwwrrrwwwrrwwwrrwrrwwrrwwwww-\n"
+        strike += "wwwwwwwwwwwwwrrwrrwrrwwwrrwwwrrrrwwwrrrrwww-\n"
+        strike += "wwwwwwwwwwwwwrrwwwwrrwwwrrwwwrrwrrwwrrwwwww-\n"
+        strike += "wwwwwwwwwwwwwrrwwwwrrwrrrrrrwrrwwrrwrrrrrrw-\n"
+    elif r==1:
+        strike += "wrrrrrwwwrrrrwwwrrrrwwrrrrrwwrrrrrwwwrrrrrw-\n"
+        strike += "wrrwwrrwrrwwrrwrrwwrrwrrwwrrwrrwwrrwrrwwwww-\n"
+        strike += "wrrrrrwwrrwwrrwrrrrrrwrrrrrrwrrwwrrwwrrrrww-\n"
+        strike += "wrrwwrrwrrwwrrwrrwwrrwrrwrrwwrrwwrrwwwwwrrw-\n"
+        strike += "wrrrrrwwwrrrrwwrrwwrrwrrwwrrwrrrrrwwrrrrrww-\n"
+    else:
+        strike += "wrrrrrrwrrrrrrwrrrrrwwrrrrrrwrrwwrrwrrrrrrw-\n"
+        strike += "wrrwwwwwwwrrwwwrrwwrrwwwrrwwwrrwrrwwrrwwwww-\n"
+        strike += "wrrrrrrwwwrrwwwrrrrwwwwwrrwwwrrrrwwwrrrrwww-\n"
+        strike += "wwwwwrrwwwrrwwwrrwrrwwwwrrwwwrrwrrwwrrwwwww-\n"
+        strike += "wrrrrrrwwwrrwwwrrwwrrwrrrrrrwrrwwrrwrrrrrrw-\n"
+    strike += "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww-\n"
 
-r = randint(0,99)
+    n = randint(0,1)
+    if n == 1:
+        strike += "---------------------g----------------------\n"
+        strike += "---------------------g----------------------\n"
+        strike += "---------------------g------000000----------\n"
+        strike += "---------------------g------000000----------\n"
+        strike += "---------------------g----0000000000--------\n"
+        strike += "---------------------g----0000000000--------\n"
+        strike += "---------------------g----0000000000--------\n"
+        strike += "---------------------g----0000000000--------\n"
+        strike += "---------------------g----0000000000--------\n"
+        strike += "---------------------g----0000000000--------\n"
+        strike += "---------------------g------000000----------\n"
+        strike += "---------------------g------000000----------\n"
+        strike += "---------------------g--------00------------\n"
+        strike += "---------------------g--------00------------\n"
+        strike += "---------------------0000000000000----------\n"
+        strike += "---------------------0000000000000----------\n"
+        strike += "------------------------------00--00--------\n"
+        strike += "------------------------------00--00--------\n"
+        strike += "------------------------------00--00--------\n"
+        strike += "------------------------------00--00--------\n"
+        strike += "------------------------------00--00--------\n"
+        strike += "------------------------------00--00--------\n"
+        strike += "------------------------------00------------\n"
+        strike += "------------------------------00------------\n"
+        strike += "------------------------------00------------\n"
+        strike += "------------------------------00------------\n"
+        strike += "----------------------------000000----------\n"
+        strike += "----------------------------000000----------\n"
+        strike += "----------------------------00--00----------\n"
+        strike += "----------------------------00--00----------\n"
+        strike += "----------------------------00--00----------\n"
+        strike += "----------------------------00--00----------\n"
+        strike += "----------------------------00--00----------\n"
+        strike += "----------------------------00--00----------\n"
+        strike += "--------------------------0000--0000--------\n"
+        strike += "--------------------------0000--0000--------"
+    else:
+        strike += "---------------------g----------------------\n"
+        strike += "---------------------g----------------------\n"
+        strike += "---------000000------g----------------------\n"
+        strike += "---------000000------g----------------------\n"
+        strike += "-------0000000000----g----------------------\n"
+        strike += "-------0000000000----g----------------------\n"
+        strike += "-------0000000000----g----------------------\n"
+        strike += "-------0000000000----g----------------------\n"
+        strike += "-------0000000000----g----------------------\n"
+        strike += "-------0000000000----g----------------------\n"
+        strike += "---------000000------g----------------------\n"
+        strike += "----------00000------g----------------------\n"
+        strike += "-----------00--------g----------------------\n"
+        strike += "-----------00--------g----------------------\n"
+        strike += "---------0000000000000----------------------\n"
+        strike += "---------0000000000000----------------------\n"
+        strike += "-------00--00-------------------------------\n"
+        strike += "-------00--00-------------------------------\n"
+        strike += "-------00--00-------------------------------\n"
+        strike += "-------00--00-------------------------------\n"
+        strike += "-------00--00-------------------------------\n"
+        strike += "-----------00-------------------------------\n"
+        strike += "-----------00-------------------------------\n"
+        strike += "-----------00-------------------------------\n"
+        strike += "-----------00-------------------------------\n"
+        strike += "---------000000-----------------------------\n"
+        strike += "---------000000-----------------------------\n"
+        strike += "---------00--00-----------------------------\n"
+        strike += "---------00--00-----------------------------\n"
+        strike += "---------00--00-----------------------------\n"
+        strike += "---------00--00-----------------------------\n"
+        strike += "---------00--00-----------------------------\n"
+        strike += "---------00--00-----------------------------\n"
+        strike += "-------0000--0000---------------------------\n"
+        strike += "-------0000--0000---------------------------"
 
-if r==0:
-    strike[49*1:49*6] = mike
-elif r==1:
-    strike[49*1:49*6] = boards    
+    if randint(1,40) == 1:
+        strike = "\n".join([i[::-1] for i in strike.split("\n")][::-1])
 
-strikers = [strike,strike2,"\n".join([i[::-1] for i in strike.split("\n")][::-1])]
+    return choice("rgbcmy").join(strike.split("0"))
+
+"""strikers = [strike,strike2,
+"\n".join([i[::-1] for i in strike.split("\n")][::-1])]
 for i in range(1,30):
     strikers.append("\n".join(["--"*i+j for j in strike.split("\n")]))
     strikers.append("\n".join(["--"*i+j for j in strike2.split("\n")]))
 for i in range(1,15):
     strikers.append("\n".join([j[2*i:] for j in strike.split("\n")]))
-    strikers.append("\n".join([j[2*i:] for j in strike2.split("\n")]))
+    strikers.append("\n".join([j[2*i:] for j in strike2.split("\n")]))"""
+
 
 class StrikePage(Page):
     def __init__(self):
@@ -125,6 +127,9 @@ class StrikePage(Page):
         self.background_loaded = True
 
     def generate_content(self):
-        s = choice(strikers)
-        s = choice("rgbcmy").join(s.split("0"))
-        self.print_image(s)
+        s = make_strike_one()
+        t = make_strike_one()
+        u = make_strike_one()
+        n = randint(0,15)
+        s = "\n".join([j[2*n:] for j in s.split("\n")])
+        self.print_image(join(s,join(t,u)))
