ffe92376fcd9470e6dadf345f32817a6cb887345
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-03-04 22:06:50 +0000

    Added new files

---
+++ tweet_handler.py
@@ -0,0 +1,18 @@
+import twitter
+import config
+
+class NoTwitter(BaseException):
+    pass
+
+def login():
+    try:
+        twitter.Twitter(auth=twitter.OAuth(config.twitter_access_key,
+            config.twitter_access_secret, config.twitter_consumer_key, config.twitter_consumer_secret))
+    except:
+        raise NoTwitter
+
+def search(q):
+    twapi = login()
+
+    results = twapi.search.tweets(q=q, result_type="recent")
+    return results["statuses"]
