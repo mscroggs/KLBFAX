ade57f75a6a767eeef71c4b8cadebc5c0949276c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-03-29 13:45:29 +0100

    datetime

--- config.py
+++ config.py
@@ -71,5 +71,5 @@ import pytz
 timezone = pytz.timezone('Europe/London')
 
 def now():
-    return pytz.utc.localize(datetime.now()).astimezone(timezone)
+    return datetime.now() #pytz.utc.localize(datetime.now()).astimezone(timezone)
 
