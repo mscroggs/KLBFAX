c74d8a33ea8d8411b48ec44167bf5aa58e42d15d
TD Dang <td.dang@hotmail.com>
2018-06-09 00:55:51 +0100

    charing cross added

--- pages/trains.py
+++ pages/trains.py
@@ -213,8 +213,9 @@ train09 = TrainPage("859","London King's Cross","KGX")
 train10 = TrainPage("860","London St Pancras","STP")
 train11 = TrainPage("861","London Euston","EUS")
 train12 = TrainPage("862","London Victoria","VIC")
-train13 = TrainPage("863","Cambridge","CBG")
-train14 = TrainPage("864","Cambridge","CBG")
+train13 = TrainPage("863","Charing Cross","CHX")
+train14 = TrainPage("863","Cambridge","CBG")
+
 
 class TVIPage(Page):
     def __init__(self):
