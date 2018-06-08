ba7efc6071a558acb7a701a8e7f01eed711ea57e
TD Dang <td.dang@hotmail.com>
2018-06-08 14:41:49 +0100

    delete

--- pages/ucl.py
+++ pages/ucl.py
@@ -5,7 +5,6 @@ class UCLPage(Page):
         super(UCLPage, self).__init__("211")
         self.title = " Library Spaces"
         self.tagline = "Does UCL have a space problem?"
-        self.station = station
         self.importance = 5
 
     def generate_content(self):
