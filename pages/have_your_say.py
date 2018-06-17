47fa2dcfa884b94a3c484228c82502f0d067ec6d
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-06-17 17:03:01 +0100

    Fiddling with HAve Your Say page

--- pages/have_your_say.py
+++ pages/have_your_say.py
@@ -6,8 +6,10 @@ class TwitterPage(Page):
     def __init__(self, page_num, n, tpage=None):
         if tpage is None:
             self.tpage = self
+            self.main = True
         else:
             self.tpage = tpage
+            self.main = False
         self.start_n = n
         super(TwitterPage, self).__init__(page_num)
         self.title = "Have Your Say"
@@ -18,39 +20,40 @@ class TwitterPage(Page):
             self.importance = 2
             self.in_index = False
         self.lines = []
-        self.tweet = "Tweet @EMFFAX to have your comment appear here"
+        self.tagline = "Tweet @EMFFAX to have your comment appear here"
 
     def background(self):
-        from helpers import tweet_handler
+        if self.main:
+            from helpers import tweet_handler
 
-        try:
-            results = tweet_handler.search("@EMFFAX")
-        except tweet_handler.NoTwitter:
-            self.lines = ["Twitter login failed..."]
-            return
+            try:
+                results = tweet_handler.search("@EMFFAX")
+            except tweet_handler.NoTwitter:
+                self.lines = ["Twitter login failed..."]
+                return
 
-        self.lines = []
+            self.lines = []
 
-        for tweet in results:
-            col = choice(non_dark_colors)
-            text = tweet["text"]
-            while "http" in text:
-                tsp = text.split("http",1)
-                text = tsp[0] + "<url>"
-                if " " in tsp[1]:
-                    text += tsp[1].split(" ",1)[1]
-            if text[:8] == "@EMFFAX ":
-                text = text[8:]
-            text = text.replace("@EMFFAX","EMFFAX")
-            for i in range(0,len(text),80):
-                self.lines.append((text[i:i+80],col,False))
-            self.lines.append(("@" + tweet["user"]["screen_name"] + " " + " ".join(tweet["user"]["created_at"].split(" ")[:4]),col,True))
-            self.lines.append(("",col,True))
+            for tweet in results:
+                col = choice(non_dark_colors)
+                text = tweet["text"]
+                while "http" in text:
+                    tsp = text.split("http",1)
+                    text = tsp[0] + "<url>"
+                    if " " in tsp[1]:
+                        text += tsp[1].split(" ",1)[1]
+                if text[:8] == "@EMFFAX ":
+                    text = text[8:]
+                text = text.replace("@EMFFAX","EMFFAX")
+                for i in range(0,len(text),80):
+                    self.lines.append((text[i:i+80],col,False))
+                self.lines.append(("@" + tweet["user"]["screen_name"] + " " + " ".join(tweet["user"]["created_at"].split(" ")[:4]),col,True))
+                self.lines.append(("",col,True))
 
     def generate_content(self):
         self.add_title("Have your say",font="size4")
         self.add_newline()
-        for line, col, fg in self.lines[21*self.start_n:21*(self.start_n+1)]:
+        for line, col, fg in self.tpage.lines[21*self.start_n:21*(self.start_n+1)]:
             if fg:
                 self.add_text(line, fg=col)
             else:
