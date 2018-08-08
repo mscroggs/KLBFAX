1d5ecce3c9c352adcf9f14da086f4346cfe0db45
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-08-08 16:28:24 +0100

    fix time

--- helpers/time.py
+++ helpers/time.py
@@ -3,4 +3,5 @@ import pytz as _pytz
 import config
 
 def datetime(*args, **kwargs):
-    return _pytz.utc.localize(_dt(*args, **kwargs)).astimezone(config.timezone)
+    #return _pytz.utc.localize(_dt(*args, **kwargs)).astimezone(config.timezone)
+    return _dt(*args, **kwargs)
