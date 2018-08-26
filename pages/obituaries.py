8f21169a5a6a38b734eb038b0252b187469cf0ba
Pecnut <pecnut@hotmail.com>
2018-08-26 17:58:01 +0100

    better obits page

--- pages/obituaries.py
+++ pages/obituaries.py
@@ -3,30 +3,16 @@ from ceefax import Ceefax
 import config
 
 class ObitPage(Page):
-    def __init__(self, n):
+    def __init__(self, n, which_number):
         super(ObitPage, self).__init__(n)
         self.importance = 4
         self.title = "Obituaries"
         self.index_num = "260-269"
+        self.which_number = which_number
 
-    def generate_content(self):
-        self.print_image(
-            "-yyyyyyyy--------yyyyyy--------yyyyyyyyy-------yyyyyyyyy----yyyyyyyyy----------\n"
-            "-yyyyyyyy--------yyyyyy--------yyyyyyyyy-------yyyyyyyyy----yyyyyyyyy----------\n"
-            "-ybbbbbby----------ybby----------ybbybby---------ybbbbyy------ybbbbby----------\n"
-            "-ybbyybby-yyyyyyyy-ybby-yyyyyyyy-ybbybby-yyyyyyy-ybbybby-yyyy-ybbyyyy-yyyyyyyy-\n"
-            "-ybbyybby-yyyyyyyy-ybby-yyyyyyyy-ybbybby-yyyyyyy-ybbbbyy-yyyy-ybbbbyy-yyyyyyyy-\n"
-            "-ybbyybby-ybbbbbyy-ybby-ybbbbbby-ybbybby-yybbbyy-ybbybby-ybby-ybbyyyy-ybbbbbyy-\n"
-            "-ybbbbbby-ybbyybby-ybby-yyybbyyy-ybbbbby-ybbybby-ybbybby-ybby-ybbbbby-ybbyyyyy-\n"
-            "-yyyyyyyy-ybbbbbyy-yyyy-yyybbyyy-yyyyyyy-ybbbbby-yyyyyyy-ybby-yyyyyyy-ybbbbbyy-\n"
-            "-yyyyyyyy-ybbyybby-yyyy-yyybbyyy-yyyyyyy-ybbybby-yyyyyyy-ybby-yyyyyyy-yyyybbyy-\n"
-            "----------ybbbbbyy------yyybbyyy---------ybbybby---------ybby---------ybbbbbyy-\n"
-            "--------yyyyyyyyyy----yyyyyyyyyy-------yyyyyyyyy-------yyyyyy-------yyyyyyyyyy-\n"
-            "--------yyyyyyyyyy----yyyyyyyyyy-------yyyyyyyyy-------yyyyyy-------yyyyyyyyyy-",1)
-
+    def background(self):
         import requests
         from bs4 import BeautifulSoup
-        import random
         html = requests.get('https://en.wikipedia.org/wiki/Wikipedia%3ADatabase_reports%2FRecent_deaths')
         b = BeautifulSoup(html.text, 'lxml')
         all_people = b.find_all(name = 'tr')
@@ -41,9 +27,32 @@ class ObitPage(Page):
             birth[i] = person_data[i][2].text[0:4]
             death[i] = person_data[i][3].text[0:4]
             description[i] = person_data[i][4].text.replace("\n","")
+        self.name = name
+        self.birth = birth
+        self.death = death
+        self.description = description
 
-        i = random.randint(0,10)
-        if i == 10:
+    def generate_content(self):
+        import random
+        self.print_image(
+            "-yyyyyyyy--------yyyyyy--------yyyyyyyyy-------yyyyyyyyy----yyyyyyyyy----------\n"
+            "-yyyyyyyy--------yyyyyy--------yyyyyyyyy-------yyyyyyyyy----yyyyyyyyy----------\n"
+            "-ybbbbbby----------ybby----------ybbybby---------ybbbbyy------ybbbbby----------\n"
+            "-ybbyybby-yyyyyyyy-ybby-yyyyyyyy-ybbybby-yyyyyyy-ybbybby-yyyy-ybbyyyy-yyyyyyyy-\n"
+            "-ybbyybby-yyyyyyyy-ybby-yyyyyyyy-ybbybby-yyyyyyy-ybbbbyy-yyyy-ybbbbyy-yyyyyyyy-\n"
+            "-ybbyybby-ybbbbbyy-ybby-ybbbbbby-ybbybby-yybbbyy-ybbybby-ybby-ybbyyyy-ybbbbbyy-\n"
+            "-ybbbbbby-ybbyybby-ybby-yyybbyyy-ybbbbby-ybbybby-ybbybby-ybby-ybbbbby-ybbyyyyy-\n"
+            "-yyyyyyyy-ybbbbbyy-yyyy-yyybbyyy-yyyyyyy-ybbbbby-yyyyyyy-ybby-yyyyyyy-ybbbbbyy-\n"
+            "-yyyyyyyy-ybbyybby-yyyy-yyybbyyy-yyyyyyy-ybbybby-yyyyyyy-ybby-yyyyyyy-yyyybbyy-\n"
+            "----------ybbbbbyy------yyybbyyy---------ybbybby---------ybby---------ybbbbbyy-\n"
+            "--------yyyyyyyyyy----yyyyyyyyyy-------yyyyyyyyy-------yyyyyy-------yyyyyyyyyy-\n"
+            "--------yyyyyyyyyy----yyyyyyyyyy-------yyyyyyyyy-------yyyyyy-------yyyyyyyyyy-",1)
+
+
+
+        i = int(self.which_number)
+        #i = int(self.number)
+        if i == 0:
             self.print_image(
                 "-wwwwwww-wwwwwwwwwwwwwwwwwwwww-wwwwww-wwwwwww\n"
                 "ww-----www-----ww-----ww-----www----www--w--w\n"
@@ -65,20 +74,30 @@ class ObitPage(Page):
             self.add_newline()
         else:
             self.move_cursor(y=10,x=2)
-            self.add_title(name[i],bg="BRIGHTWHITE",fg="BLACK",font="size4")
+            self.add_title(self.name[i],bg="BRIGHTWHITE",fg="BLACK",font="size4")
             self.move_cursor(y=15,x=1)
-            self.add_text(description[i])
+            self.add_text(self.description[i])
             self.move_cursor(y=16,x=20)
-            self.add_title("          "+birth[i]+" - "+death[i],bg="YELLOW",fg="BLACK",font="size4")
+            self.add_title("          "+self.birth[i]+" - "+self.death[i],bg="YELLOW",fg="BLACK",font="size4")
+            mouth_color = random.choice(['r','c','g','y','o','p'])
             self.print_image(
                 "-wwwwwww-\n"
                 "ww-----ww\n"
                 "w--w-w--w\n"
                 "w-------w\n"
-                "w---r---w\n"
+                "w---" + mouth_color + "---w\n"
                 "w-------w\n"
                 "ww-w-w-ww\n"
                 "w-w-w-w-w",17,1)
 
 
-i_p = ObitPage("260")
+i_p0 = ObitPage("260",0)
+i_p1 = ObitPage("261",1)
+i_p2 = ObitPage("262",2)
+i_p3 = ObitPage("263",3)
+i_p4 = ObitPage("264",4)
+i_p5 = ObitPage("265",5)
+i_p6 = ObitPage("266",6)
+i_p7 = ObitPage("267",7)
+i_p8 = ObitPage("268",8)
+i_p9 = ObitPage("269",9)
